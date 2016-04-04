0f8a8662aae76fdba49a36336932abaa86d1b547
Nate Nolting <me@natenolting.com>
2016-04-04 16:29:02 -0500

    removed debug data from theme index

--- public_html/wp-content/themes/gigazone-gaming/index.php
+++ public_html/wp-content/themes/gigazone-gaming/index.php
@@ -13,6 +13,3 @@ include(locate_template('get-context.php'));
  * Output page to browser
  */
 Timber::render('pages/page.twig', $context);
-
-var_dump($context);
-//echo '<code><pre>' . print_r($context, true) . '</pre></code>';
