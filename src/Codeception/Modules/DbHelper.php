768d21de4c823374a2abbbe00ad55d6659f9b1e1
Nate Nolting <me@natenolting.com>
2016-04-05 14:47:05 -0500

    added db helper and setup dev autoloading for codeception modules

---
+++ src/Codeception/Modules/DbHelper.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * DbHelper
+ *
+ * Created 11/16/15 10:27 AM
+ * Help with the population of database with tests
+ *
+ * @author Nate Nolting <naten@paulbunyan.net>
+ * @package Codeception\Module
+ */
+
+namespace Codeception\Module;
+
+use Codeception\Module;
+
+/**
+ * Class DbHelper
+ * @package Codeception\Module
+ */
+class DbHelper extends Module
+{
+
+    /**
+     * @param \AcceptanceTester|\FunctionalTester $I
+     */
+    public function populateDatabase($I, array $config = [])
+    {
+        //\Codeception\Util\Debug::debug($config);
+        $root = dirname(dirname(dirname(__DIR__)));
+        $I->runShellCommand('mysql -h "'.$config['server'].'" -u "'.$config['user'].'" "-p'.$config['password'].'" "'.$config['database'].'" < '.$root.'/'.$config['dump'] . ' 2> nul');
+    }
+
+}
