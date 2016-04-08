34e0c274b01285e0b5614555cf81b68b4f321080
Nate Nolting <me@natenolting.com>
2016-04-08 09:25:22 -0500

    added resetting the migrations after tests run

--- testing.sh
+++ testing.sh
@@ -3,8 +3,11 @@ log="codecept-result.log"
 finished=false
 touch ${log}
 echo -n "" >${log}
+# run tests inside vagrant box
 vagrant ssh -c "cd /var/www; php artisan config:clear; php artisan migrate; php codecept.phar clean; php codecept.phar build; php codecept.phar run --coverage-html --coverage-xml;" > ${log} 2>&1
 cat ${log}
+# reset the migrations in the box
+vagrant ssh -c "cd /var/www; php artisan migrate;"
 if grep "FAILURES!" ${log}
     then
        echo "TESTS FAILED!"; exit 1;
