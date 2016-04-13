49668d062a511afa1eeb51c9288361c1066edb19
Nate Nolting <me@natenolting.com>
2016-04-13 16:30:32 -0500

    fixed path in loading autoloader

--- public_html/wp-content/themes/gigazone-gaming/images/icons/diamond.php
+++ public_html/wp-content/themes/gigazone-gaming/images/icons/diamond.php
@@ -1,7 +1,9 @@
 <?php
 use GigaZone\Svg\Svg;
-require_once '../../../../../vendor/autoload.php';
-Svg::header();
+require_once realpath('../../../../../../vendor/autoload.php');
+if(!isset($_GET['debug']) || filter_var($_GET['debug'], FILTER_VALIDATE_BOOLEAN) === false) {
+    Svg::header();
+}
 $content = file_get_contents(__DIR__ . '/diamond.svg');
 Svg::fillColor(isset($_GET['bg']) ? $_GET['bg'] : '', '000000', $content);
 die($content);
