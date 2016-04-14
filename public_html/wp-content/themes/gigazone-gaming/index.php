3cb1c062144b25bffc02708d0145e684d4ce4640
Nate Nolting <me@natenolting.com>
2016-04-14 15:24:30 -0500

    added check to see if pager should be locked on the splash page or allow internal pages to be viewed

--- public_html/wp-content/themes/gigazone-gaming/index.php
+++ public_html/wp-content/themes/gigazone-gaming/index.php
@@ -12,8 +12,19 @@ include(locate_template('get-context.php'));
 /**
  * Output page to browser
  */
+// if the WP_FRONT_PAGE_ONLY flat is true then relay all requests to the front page post
+if(filter_var(getenv('WP_FRONT_PAGE_ONLY'), FILTER_VALIDATE_BOOLEAN) === true) {
+    $homeId = get_option('page_on_front');
+    $context['page'] = Timber::get_post(get_option('page_on_front'));
+    $context['body_class'] = 'home page page-id-'.$homeId.' page-template-default';
+    Timber::render('pages/splash.twig', $context);
+    die();
+}
+
 if(is_front_page()) {
-    Timber::render('pages/home.twig', $context);   
+    Timber::render('pages/splash.twig', $context);   
 } else {
     Timber::render('pages/page.twig', $context);
 }
+
+//echo '<code><pre>' . print_r(Timber::get_context(), true) . '</pre></code>';
