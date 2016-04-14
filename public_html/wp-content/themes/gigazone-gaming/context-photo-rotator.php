cc5eb52f5ae3e5cd0bc1223780a7f7a5608850cc
Nate Nolting <me@natenolting.com>
2016-04-14 15:24:51 -0500

    added check if see if page key is set

--- public_html/wp-content/themes/gigazone-gaming/context-photo-rotator.php
+++ public_html/wp-content/themes/gigazone-gaming/context-photo-rotator.php
@@ -5,8 +5,10 @@ $context['masthead_photo_rotator'] = [];
  * media tags plugin is installed and there's
  * images in the current page
  */
-if (function_exists('get_attachments_by_media_tags') && is_object($context['page']) && property_exists($context['page'],
-        'slug')
+if (function_exists('get_attachments_by_media_tags') 
+    && isset($context['page']) 
+    && is_object($context['page']) 
+    && property_exists($context['page'], 'slug')
 ) {
     $rotatorImages = get_attachments_by_media_tags('media_tags=' . $context['page']->slug . '-masthead-photo-rotator&media_types=jpeg,jpg,gif,png&orderby=post_title&order=DESC');
     if ($rotatorImages) {
