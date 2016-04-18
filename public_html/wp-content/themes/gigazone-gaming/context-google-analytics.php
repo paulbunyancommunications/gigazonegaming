ab22c11b78cc8849721d459e36a5b4cffa985142
Nate Nolting <me@natenolting.com>
2016-04-18 07:54:20 -0500

    only check for production state when loading analytics

--- public_html/wp-content/themes/gigazone-gaming/context-google-analytics.php
+++ public_html/wp-content/themes/gigazone-gaming/context-google-analytics.php
@@ -1,6 +1,4 @@
 <?php
-if (stripos(TimberURLHelper::get_host(), parse_url(getenv('APP_URL_PRODUCTION'), PHP_URL_HOST))
-    && getenv('APP_ENV') === 'production'
-) {
+if (getenv('APP_ENV') === 'production') {
     $context['google_analytics'] = Timber::compile('partials/google-analytics.twig', ['key' => 'UA-76362808-1']);
 }
