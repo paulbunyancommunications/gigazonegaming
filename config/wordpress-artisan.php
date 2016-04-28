2b0ae07ba8c252b6625146f7eb4b3bddac0b563d
Nate Nolting <me@natenolting.com>
2016-04-28 07:48:43 -0500

    Pulled in wordpress-artisan package which replaces existing wordpress artisan commands

---
+++ config/wordpress-artisan.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    /*
+     * --------------------------------------------------------------------------
+     * Path to the root directory
+     * --------------------------------------------------------------------------
+     *
+     * This option determines the path to the root directory, it's where
+     * the package will be looking for the env file. This file is
+     * usually located in the root but you may change that.
+     */
+
+    'path' => realpath(base_path()),
+
+    /*
+     * --------------------------------------------------------------------------
+     * Path to the public directory
+     * --------------------------------------------------------------------------
+     *
+     * This option determines the path to the public directory, it's where
+     * the package will be looking for the document root. This is
+     * usually located in public but you may change that.
+     */
+
+    'public_path' => realpath(base_path('public_html')),
+    
+    /*
+     * --------------------------------------------------------------------------
+     * Environment file name
+     * --------------------------------------------------------------------------
+     *
+     * This option determines the name or the env file. This file is
+     * usually named .env you may change that.
+     */
+
+    'env-file' =>'.env', 
+    
+    /*
+     * --------------------------------------------------------------------------
+     * Wordpress maintenance file
+     * --------------------------------------------------------------------------
+     *
+     * This option determines the name or the maintenance file. This file is
+     * usually named .maintenance you may change that.
+     */
+
+    'wp-maintenance-file' =>'wp/.maintenance',
+];
--- public_html/wp-content/themes/gigazone-gaming/functions.php
+++ public_html/wp-content/themes/gigazone-gaming/functions.php
@@ -10,5 +10,3 @@ $cacheDriver = new Stash\Driver\FileSystem(['path' => dirname($_SERVER['DOCUMENT
 add_shortcode('update-sign-up', [$bootstrap, 'updateSignUpFormShortCode']);
 add_shortcode('bloginfo', [$bootstrap, 'blogInfoShortCode']);
 
-add_action( 'init', [$bootstrap, 'maintenance_mode']);
-
