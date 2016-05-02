4859f150cc8fc92faca5d829c512de76fdeee935
Nate Nolting <me@natenolting.com>
2016-05-02 14:49:23 -0500

    updated sekenium provisioner

--- puphpet/files/startup-always/selenium-provision.sh
+++ puphpet/files/startup-always/selenium-provision.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -e
-if [ -e /.s-provisioned ]; then
-    echo 'Selenium provisioning already ran.'
-else
-    cd /usr/local
-    sudo wget -O selenium-provision.sh https://gist.githubusercontent.com/natenolting/10bb838d32a2078d56a4/raw/1c80d3525a4841f0498f8b127d8befa8dc8034ff/provision-selenium.sh
-    sudo sh selenium-provision.sh
-    sudo touch /.s-provisioned
-fi
+cd ~/
+seleniumScript="provision_selenium.sh"
+seleniumScriptPath="${PWD}/${seleniumScript}"
+seleniumScriptDownload="https://raw.githubusercontent.com/paulbunyannet/bash/master/selenium/${seleniumScript}"
+if [ -f "${seleniumScriptPath}" ]; then
+    sudo rm -f ${seleniumScriptPath}
+fi;
+sudo wget -O ${seleniumScriptPath} ${seleniumScriptDownload}
+. ${seleniumScriptPath}
