e6b53b2822fad4d35d0f73de7e320df45152d7da
Nate Nolting <me@natenolting.com>
2016-05-12 12:26:24 -0500

    Added push script

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+if [ -z "$1" ]; then
+    echo "A commit message is required!"; exit 1;
+fi;
+
+message="$1"
+#http://stackoverflow.com/a/12142066/405758
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$branch" -eq "master" ]; then
+    echo "You should not be doing commits directly to the ${branch} branch!"; exit 1;
+fi;
+
+git commit -m "${message}"
+
+if [ "$branch" == "develop" ]; then
+    git checkout master
+    git merge develop
+fi
+git push
+git checkout develop
