02e8d7d5a48dcf7b7e501d57a0c05ef1da2a4727
Nate Nolting <me@natenolting.com>
2016-05-18 12:03:07 -0500

    updated sign up and contact forms

--- database/migrations/2016_05_10_152845_add_contact_form_to_contact_page.php
+++ database/migrations/2016_05_10_152845_add_contact_form_to_contact_page.php
@@ -15,8 +15,8 @@ class AddContactFormToContactPage extends Migration
     {
         $page = DB::table('wp_posts')->where('post_name', 'contact-us')->first();
         $page = App\Models\WpPost::find($page->ID);
-        $contentString = '[contact-us new_line="," delimiter="|" questions="Please list any comments or suggestions.|textarea,Your Name|text,Your Email Address|email" inputs="your-name|name,your-email-address|email"]Have comments or questions, please let us know![/contact-us]';
-        if($page && !preg_match($this->shortCode, $page->post_content, $matches)) {
+        $contentString = '[contact-us new_line="," delimiter="|" questions="Please list any comments or suggestions.|textarea,Your Name|text,Your Email Address|email,Sign up for updates|boolean" inputs="your-name|name,your-email-address|email,sign-up-for-updates|update-recipient,please-list-any-comments-or-suggestions|comment"]Have comments or questions, please let us know![/contact-us]';
+        if ($page && !preg_match($this->shortCode, $page->post_content, $matches)) {
             $page->post_content = $page->post_content . $contentString;
             $page->save();
         }
--- database/migrations/2016_05_12_173940_make_team_sign_up_post.php
+++ database/migrations/2016_05_12_173940_make_team_sign_up_post.php
@@ -12,7 +12,7 @@ class MakeTeamSignUpPost extends Migration
     public function up()
     {
         $post = DB::table('wp_posts')
-            ->where('post_name', 'team-sign-up')
+            ->where('post_name', 'lol-team-sign-up')
             ->where('post_type', 'post')
             ->first();
         $parent = DB::table('wp_posts')
@@ -21,9 +21,9 @@ class MakeTeamSignUpPost extends Migration
             ->first();
         if (!$post) {
             $now = date("Y-m-d H:i:s");
-            $query = "INSERT INTO `wp_posts` (`post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`)
-VALUES
-	(1, '". $now ."', '". $now ."', '[team-sign-up new_line=\",\" delimiter=\"|\" questions=\"Team Captain,Team Captain LOL Username,Team Captain Email Address|email,Team Captain Phone|tel,Teammate One LOL Username,Teammate One Email Address|email,Teammate Two LOL Username,Teammate Two Email Address|email,Teammate Three LOL Username,Teammate Three Email Address|email,Teammate Four LOL Username,Teammate Four Email Address|email\" inputs=\"team-captain|name,team-captain-email-address|email\" headings=\"Team Captain|team-captain,Team Members|teammate-one-lol-username\"]Please fill out the form below to let us know that your are interested in participating in the event[/team-sign-up]', 'Team Sign Up', '', 'publish', 'open', 'open', '', 'team-sign-up', '', '', '". $now ."', '". $now ."', '', ".$parent->ID.", 'http://gigazonegaming.local/auto-draft/', 0, 'post', '', 0);";
+            $query = 'INSERT INTO `wp_posts` (`post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`)
+                      VALUES
+	                  (1, \''.$now.'\', \''.$now.'\', \'[team-sign-up new_line="," delimiter="|" questions="update-recipient|hidden|yes,participate|hidden|yes,Team Captain,Team Captain LOL Summoner Name,Team Captain Email Address|email,Team Captain Phone|tel,Teammate One LOL Summoner Name,Teammate One Email Address|email,Teammate Two LOL Summoner Name,Teammate Two Email Address|email,Teammate Three LOL Summoner Name,Teammate Three Email Address|email,Teammate Four LOL Summoner Name,Teammate Four Email Address|email" inputs="team-captain|name,team-captain-email-address|email" headings="Team Captain|team-captain,Team Members|teammate-one-lol-summoner-name"]Please fill out the form below to let us know that your are interested in participating in the event[/team-sign-up]\', \'League of Legends Team Sign Up\', \'\', \'publish\', \'open\', \'open\', \'\', \'lol-team-sign-up\', \'\', \'\', \''.$now.'\', \''.$now.'\', \'\', '.$parent->ID.', \'http://gigazonegaming.local/auto-draft/\', 0, \'post\', \'\', 0);';
             DB::insert($query);
         }
     }
