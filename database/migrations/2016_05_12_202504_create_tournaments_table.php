458873b7c3c245250d1ecd90a7f1d254def0fd92
Nate Nolting <me@natenolting.com>
2016-05-18 11:58:47 -0500

    added migration for tournament table

---
+++ database/migrations/2016_05_12_202504_create_tournaments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql_champ')->create('tournaments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('token');
+            $table->integer('game_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('mysql_champ')->drop('tournaments');
+    }
+}
