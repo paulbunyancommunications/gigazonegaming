c383d65f56b60dc4fe442d47e74ad192a5a98db1
Nate Nolting <me@natenolting.com>
2016-05-23 13:10:44 -0500

    updated res too use factory to generate tournament

--- tests/functional/TeamSignUpTest.php
+++ tests/functional/TeamSignUpTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\Functional;
 
+use App\Models\Championship\Tournament;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Pbc\Bandolier\Type\Numbers;
 
@@ -19,14 +20,17 @@ class TeamSignUpTest extends \TestCase
      */
     public function it_returns_a_success_message_from_lol_team_registration_route()
     {
+
         $faker = \Faker\Factory::create();
+        $name = implode('-',$faker->words());
+        $tournament = factory(Tournament::class)->create(['name' => $name]);
         $parameters = [
             'email' => $faker->email,
             'name' => $faker->name,
             'team-name' => $faker->company,
             'team-captain-lol-summoner-name' => $faker->userName,
             'team-captain-phone' => $faker->phoneNumber,
-            'tournament' => 'gigazone-gaming-2016-league-of-legends',
+            'tournament' => $name,
             'fields' => ['email','name','team-name','team-captain-lol-summoner-name','team-captain-phone']
         ];
         for ($i = 1; $i <= 4; $i++) {
