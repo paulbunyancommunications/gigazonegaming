2320562d8d34bb5bab5b82b0cefe2884642c14c2
Nelson <manklar@hotmail.com>
2016-05-23 20:40:45 -0500

    team view working

--- resources/views/game/team.blade.php
+++ resources/views/game/team.blade.php
@@ -7,18 +7,19 @@
     }
 @endsection
 @section('content')
-    @if(isset($games) || $games != [])
-        <ul id="listOfTournaments" class="listing">
-            @if(!isset($tournaments) || $tournaments == [])
-                <li>There are no Tournaments yet</li>
+
+    @if(isset($tournaments) || $tournaments != [])
+        <ul id="listOfTeams" class="listing">
+            @if(!isset($teams) || $teams == [])
+                <li>There are no Teams yet</li>
             @else
-                @foreach($tournaments as $id => $tournament)
+                @foreach($teams as $id => $team)
                     <li>
-                        {{ Html::linkAction('Backend\Manage\TournamentsController@index', $tournament["name"], array('class' => 'btn btn-default list')) }}
+                        {{ Html::linkAction('Backend\Manage\TeamsController@index', $team["name"], array('class' => 'btn btn-default list')) }}
                         &nbsp;&nbsp;
-                        {{ Html::linkAction('Backend\Manage\TournamentsController@edit', 'Edit', array('tournament_id'=>$tournament["id"]), array('class' => 'btn btn-success list fa fa-pencil-square-o')) }}
+                        {{ Html::linkAction('Backend\Manage\TeamsController@edit', 'Edit', array('team_id'=>$team["id"]), array('class' => 'btn btn-success list fa fa-pencil-square-o')) }}
                         &nbsp;&nbsp;
-                        {{ Form::open(array('id' => "tournamentForm".$tournament["id"], 'action' => array('Backend\Manage\TournamentsController@destroy', $tournament["id"]), 'class' => "deletingForms")) }}
+                        {{ Form::open(array('id' => "teamForm".$team["id"], 'action' => array('Backend\Manage\TeamsController@destroy', $team["id"]), 'class' => "deletingForms")) }}
                         <input name="_method" type="hidden" value="DELETE">
                         {!!
                             Form::submit(
@@ -41,31 +42,31 @@
             </div>
         @endif
         @if(isset($cont_updated) and  $cont_updated)
-            <div class="alert alert-success"><strong>Success!</strong> You have updated this Tournament.</div>
+            <div class="alert alert-success"><strong>Success!</strong> You have updated this Team.</div>
         @endif
-        @if(isset($theTournament->name))
-            {{ Form::open(array('id' => "tournamentForm", 'action' => array('Backend\Manage\TournamentsController@update', $theTournament->id))) }}
+        @if(isset($theTeam->name))
+            {{ Form::open(array('id' => "teamForm", 'action' => array('Backend\Manage\TeamsController@update', $theTeam->id))) }}
         @else
-            {{  Form::open(array('id' => "tournamentForm", 'action' => array('Backend\Manage\TournamentsController@create'))) }}
+            {{  Form::open(array('id' => "teamForm", 'action' => array('Backend\Manage\TeamsController@create'))) }}
         @endif
 
         <div class="form-group">
-            @if(isset($theTournament->name))
+            @if(isset($theTeam->name))
                 <input name="_method" type="hidden" value="PUT">
             @else
                 <input name="_method" type="hidden" value="POST">
             @endif
             <div class="form-group">
-                <label for="name">Tournament Name: </label> &nbsp; <input type="text" name="name" id="name" placeholder="The name of the tournament" @if(isset($theTournament->name))value="{{$theTournament->name}}"@endif/>
+                <label for="name">Team Name: </label> &nbsp; <input type="text" name="name" id="name" placeholder="The name of the team" @if(isset($theTeam->name))value="{{$theTeam->name}}"@endif/>
             </div>
             <div class="form-group">
-                <label for="game_id">Tournament Game ID: </label> &nbsp;
-                <select type="text" name="game_id" id="game_id" >
-                    @foreach($games as $key => $game)
-
-                        <option value="{{$game['id']}}"
-                        @if(isset($theTournament['game_id']) and $theTournament['game_id'] == $game['id']) selected @endif
-                        >{{ $game['name'] }}</option>
+                <label for="emblem">Team Emblem: </label> &nbsp; <input type="text" name="emblem" id="emblem" placeholder="The url to the emblem of the team" @if(isset($theTeam->emblem))value="{{$theTeam->emblem}}"@endif/>
+            </div>
+            <div class="form-group">
+                <label for="tournament_id">Team Tournament ID: </label> &nbsp;
+                <select type="text" name="tournament_id" id="tournament_id" >
+                    @foreach($tournaments as $key => $tournament)
+                        <option value="{{$tournament['id']}}" @if(isset($theTeam['tournament_id']) and $theTeam['tournament_id'] == $tournament['id']) selected @endif>{{ $tournament['name'] }}</option>
                     @endforeach
                 </select>
             </div>
@@ -74,13 +75,13 @@
             </div>
             <div class="form-group">
                 <input type="submit" name="submit" id="submit" class='btn btn-default' value="Save">
-                {{ Html::link('/manage/tournament/', 'Clear', array('id' => 'reset', 'class' => 'btn btn-default'))}}
+                {{ Html::link('/manage/team/', 'Clear', array('id' => 'reset', 'class' => 'btn btn-default'))}}
             </div>
         </div>
         </form>
     @else
-        <h1>Sorry, no games where found on the database!, please create a game before proceding with a tournament</h1>
-        {{ Html::link('/manage/game/', 'Create a Game', array('id' => 'new_game', 'class' => 'btn btn-default'))}}
+        <h1>Sorry, no tournaments where found on the database!, please create a tournament before proceding with a team</h1>
+        {{ Html::link('/manage/tournament/', 'Create a Tournament', array('id' => 'new_tournament', 'class' => 'btn btn-default'))}}
     @endif
 
 @endsection
