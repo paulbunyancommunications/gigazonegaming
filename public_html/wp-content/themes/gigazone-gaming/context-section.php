4e0fe4da69109e0efc60944c8fc90bd1d93df7fa
Nate Nolting <me@natenolting.com>
2016-05-28 01:23:03 -0500

    updated section context in theme

--- public_html/wp-content/themes/gigazone-gaming/context-section.php
+++ public_html/wp-content/themes/gigazone-gaming/context-section.php
@@ -1,9 +1,11 @@
 <?php
 // get section
-if(cnr_is_section()) {
+if (cnr_is_section()) {
     $context['section'] = $context['page'];
-} elseif($getSection = cnr_get_the_section()) {
+} elseif ($getSection = cnr_get_the_section()) {
     $context['section'] = Timber::get_post($getSection);
+} elseif (get_post()->post_type === 'page') {
+    $context['section'] = Timber::get_post(get_post()->ID);
 } else {
     $context['section'] = false;
 }
