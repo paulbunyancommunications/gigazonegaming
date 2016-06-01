b9b02b22893488da737a81f32c1c8843d8287823
Nelson <manklar@hotmail.com>
2016-06-01 15:20:53 -0500

    made extra functions

--- app/Providers/ChampionshipGameComposerProvider.php
+++ app/Providers/ChampionshipGameComposerProvider.php
@@ -21,41 +21,7 @@ class ChampionshipGameComposerProvider extends ServiceProvider
      */
     public function boot()
     {
-        $expiresAt = Carbon::now('CMT')->addMinute(1)->toDateTimeString(); // 1 min
-        if(Cache::has('games_c')){
-            $games = Cache::get('games_c');
-        }else{
-            $games = Game::orderBy('name')->get()->toArray();
-            Cache::put('games_c', $games, $expiresAt);
-        }
-        if(Cache::has('tournaments_c')){
-            $tournaments = Cache::get('tournaments_c');
-        }else{
-            $tournaments = Tournament::orderBy('name')->get()->toArray();
-            Cache::put('tournament_c', $tournaments, $expiresAt);
-        }
-        if(Cache::has('teams_c')){
-            $teams = Cache::get('teams_c');
-        }else{
-            $teams = Team::orderBy('name')->get()->toArray();
-            $times = Player::select(DB::raw("COUNT(id) as team_count"), "team_id")->groupBy('team_id')->get()->toArray();
-
-            foreach($teams as $key => $team){
-                foreach($times as $k => $t){
-                    if($team['id'] == $t['team_id']){
-                        $teams[$key]['team_count'] = $t['team_count'];
-                        break;
-                    }
-                }
-            }
-            Cache::put('teams_c', $teams, $expiresAt);
-        }
-        if(Cache::has('players_c')){
-            $players = Cache::get('players_c');
-        }else{
-            $players = Player::orderBy('team_id')->get()->toArray();
-            Cache::put('players_c', $players, $expiresAt);
-        }
+        list($games, $tournaments, $teams, $players) = $this->DBProcessCacheAll();
         View::composer(['game.game'],  function ($view) use ($games) {
             $view->with('games', $games);
         });
@@ -84,5 +50,58 @@ class ChampionshipGameComposerProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    /**
+     * @return array
+     */
+    public function DBProcessCacheAll()
+    {
+        $expiresAt = Carbon::now()->addMinute(1)->toDateTimeString(); // 1 min
+        if (Cache::has('games_c')) {
+            $games = Cache::get('games_c');
+        } else {
+            $games = Game::orderBy('name')->get()->toArray();
+            Cache::put('games_c', $games, $expiresAt);
+        }
+        if (Cache::has('tournaments_c')) {
+            $tournaments = Cache::get('tournaments_c');
+        } else {
+            $tournaments = Tournament::orderBy('name')->get()->toArray();
+            Cache::put('tournament_c', $tournaments, $expiresAt);
+        }
+        if (Cache::has('teams_c')) {
+            $teams = Cache::get('teams_c');
+        } else {
+            $teams = Team::orderBy('name')->get()->toArray();
+            $times = Player::select(DB::raw("COUNT(id) as team_count"), "team_id")->groupBy('team_id')->get()->toArray();
+
+            foreach ($teams as $key => $team) {
+                foreach ($times as $k => $t) {
+                    if ($team['id'] == $t['team_id']) {
+                        $teams[$key]['team_count'] = $t['team_count'];
+                        break;
+                    }
+                }
+            }
+            Cache::put('teams_c', $teams, $expiresAt);
+        }
+        if (Cache::has('players_c')) {
+            $players = Cache::get('players_c');
+            return array($games, $tournaments, $teams, $players);
+        } else {
+            $players = Player::orderBy('team_id')->get()->toArray();
+            foreach ($players as $key => $player) {
+                foreach ($teams as $k => $t) {
+                    if ($t['id'] == $player['team_id']) {
+                        $players[$key]['team_count'] = $t['team_count'];
+                        $players[$key]['team_name'] = $t['name'];
+                        break;
+                    }
+                }
+            }
+            Cache::put('players_c', $players, $expiresAt);
+            return array($games, $tournaments, $teams, $players);
+        }
     }
 }
