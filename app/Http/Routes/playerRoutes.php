1a59ec2d8183dd835d64d89384e04b48278ae851
Nelson <manklar@hotmail.com>
2016-06-01 10:41:07 -0500

    routes update

--- app/Http/Routes/playerRoutes.php
+++ app/Http/Routes/playerRoutes.php
@@ -7,8 +7,8 @@
  */
 
 Route::get('/manage/player', ['as' => 'manage.player.index', 'uses' => 'Backend\Manage\PlayersController@index']); //no need for middleware as we return the names only
+Route::post('/manage/player', ['as' => 'manage.player.filter', 'uses' => 'Backend\Manage\PlayersController@filter']);
 Route::group(['middleware' => ['WPAdmin']], function () {
-    Route::post('/manage/player', ['as' => 'manage.player.filter', 'uses' => 'Backend\Manage\PlayersController@filter']); //no need for middleware as we return the names only
     Route::get('/manage/player/edit/{player_id}', ['as' => 'manage.player.edit', 'uses' => 'Backend\Manage\PlayersController@edit']);
     Route::put('/manage/player/edit/{player_id}', ['as' => 'manage.player.edited', 'uses' => 'Backend\Manage\PlayersController@update']);
     Route::post('/manage/player/new/', ['as' => 'manage.player.new', 'uses' => 'Backend\Manage\PlayersController@create']);
