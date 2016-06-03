b9bda26568c836d116b6d2917751989f55432898
Nelson <manklar@hotmail.com>
2016-06-02 21:14:14 -0500

    still under work but making the provider for the routes better for the app

--- app/Providers/ChampionshipGameComposerProvider.php
+++ app/Providers/ChampionshipGameComposerProvider.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Championship\Game;
+use Stash\Session;
 use \View;
 use Cache;
 
@@ -21,7 +22,18 @@ class ChampionshipGameComposerProvider extends ServiceProvider
      */
     public function boot()
     {
-        list($games, $tournaments, $teams, $players) = $this->DBProcessCacheAll();
+        if(isset($flush) and $flush){
+            Cache::flush();
+            $flush = false;
+        }
+        if(isset($cache) and $cache){ //TODO: will have to update to session whenever have time
+            list($games, $tournaments, $teams, $players) = $this->DBProcessCacheAll();
+        }elseif(!isset($cache) or !$cache){
+            $games = $this->DBGetGames();
+            $tournaments = $this->DBGetTournaments();
+            $teams = $this->DBGetTeams();
+            $players = $this->DBGetPlayers($teams);
+        }
         View::composer(['game.game'],  function ($view) use ($games) {
             $view->with('games', $games);
         });
@@ -57,51 +69,94 @@ class ChampionshipGameComposerProvider extends ServiceProvider
      */
     public function DBProcessCacheAll()
     {
-        $expiresAt = Carbon::now()->addMinute(1)->toDateTimeString(); // 1 min
-        if (Cache::has('games_c')) {
+        $expiresAt = Carbon::now()->addMinute(2)->toDateTimeString(); // 1 min
+        $expiredAt = null;
+        if (!Cache::has('expiration_c') or Cache::get('expiration_c') > $expiresAt) {
+            $expiredAt = Cache::get('expiration_c');
+        } else {
+            Cache::put('expiration_c', $expiresAt, $expiresAt);
+        }
+        if (Cache::has('games_c') or $expiredAt != null) {
             $games = Cache::get('games_c');
         } else {
-            $games = Game::orderBy('name')->get()->toArray();
+            $games = $this->DBGetGames();
             Cache::put('games_c', $games, $expiresAt);
         }
-        if (Cache::has('tournaments_c')) {
+        if (Cache::has('tournaments_c') or $expiredAt != null) {
             $tournaments = Cache::get('tournaments_c');
         } else {
-            $tournaments = Tournament::orderBy('name')->get()->toArray();
+            $tournaments = $this->DBGetTournaments();
             Cache::put('tournament_c', $tournaments, $expiresAt);
         }
-        if (Cache::has('teams_c')) {
+        if (Cache::has('teams_c') or $expiredAt != null) {
             $teams = Cache::get('teams_c');
         } else {
-            $teams = Team::orderBy('name')->get()->toArray();
-            $times = Player::select(DB::raw("COUNT(id) as team_count"), "team_id")->groupBy('team_id')->get()->toArray();
-
-            foreach ($teams as $key => $team) {
-                foreach ($times as $k => $t) {
-                    if ($team['id'] == $t['team_id']) {
-                        $teams[$key]['team_count'] = $t['team_count'];
-                        break;
-                    }
-                }
-            }
+            $teams = $this->DBGetTeams();
             Cache::put('teams_c', $teams, $expiresAt);
         }
-        if (Cache::has('players_c')) {
+        if (Cache::has('players_c') or $expiredAt != null) {
             $players = Cache::get('players_c');
             return array($games, $tournaments, $teams, $players);
         } else {
-            $players = Player::orderBy('team_id')->get()->toArray();
-            foreach ($players as $key => $player) {
-                foreach ($teams as $k => $t) {
-                    if ($t['id'] == $player['team_id']) {
-                        $players[$key]['team_count'] = $t['team_count'];
-                        $players[$key]['team_name'] = $t['name'];
-                        break;
-                    }
-                }
-            }
+            $players = $this->DBGetPlayers($teams);
             Cache::put('players_c', $players, $expiresAt);
             return array($games, $tournaments, $teams, $players);
         }
+    }
+
+    /**
+     * @return array
+     */
+    public function DBGetTeams()
+    {
+        $teams = Team::orderBy('name')->get()->toArray();
+        $times = Player::select(DB::raw("COUNT(id) as team_count"), "team_id")->groupBy('team_id')->get()->toArray();
+
+        foreach ($teams as $key => $team) {
+            foreach ($times as $k => $t) {
+                if ($team['id'] == $t['team_id']) {
+                    $teams[$key]['team_count'] = $t['team_count'];
+                    break;
+                }
+            }
+        }
+        return $teams;
+    }
+
+    /**
+     * @param $teams
+     * @return mixed
+     */
+    public function DBGetPlayers($teams)
+    {
+        $players = Player::orderBy('team_id')->get()->toArray();
+        foreach ($players as $key => $player) {
+            foreach ($teams as $k => $t) {
+                if ($t['id'] == $player['team_id']) {
+                    $players[$key]['team_count'] = $t['team_count'];
+                    $players[$key]['team_name'] = $t['name'];
+                    break;
+                }
+            }
+        }
+        return $players;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function DBGetTournaments()
+    {
+        $tournaments = Tournament::orderBy('name')->get()->toArray();
+        return $tournaments;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function DBGetGames()
+    {
+        $games = Game::orderBy('name')->get()->toArray();
+        return $games;
     }
 }
