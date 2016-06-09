018d5ff985c45aed0b7278262a9c162287f02ab1
Nate Nolting <me@natenolting.com>
2016-06-09 10:15:51 -0500

    updated team signup middleware so that tournament name can be set

--- app/Http/Middleware/LolTeamSignUpMiddleware.php
+++ app/Http/Middleware/LolTeamSignUpMiddleware.php
@@ -6,16 +6,18 @@ use App\Http\Requests\LolTeamSignUpRequest;
 use App\Models\Championship\Player;
 use App\Models\Championship\Team;
 use App\Models\Championship\Tournament;
-use Pbc\Bandolier\Type\Numbers;
 use Closure;
+use Pbc\Bandolier\Type\Numbers;
 
 class LolTeamSignUpMiddleware
 {
+    protected $tournament;
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -25,13 +27,19 @@ class LolTeamSignUpMiddleware
         if ($validator->fails()) {
             return \Response::json(['error' => $validator->errors()->all()]);
         }
-
+        
+        // if tournament is not already set then get it from the request
+        if (!$this->getTournament()) {
+            $this->setTournament($request->input('tournament'));
+        }
         // get tournament by name
-        $tournament = Tournament::where('name',$request->input('tournament'))->first();
-
+        $tournament = Tournament::where('name', $this->getTournament())->get();
+        if ($tournament->isEmpty()) {
+            return \Response::json(['error' => ['Could not find tournament "' . $this->getTournament() . '"']]);
+        }
         // make new team
         $team = new Team();
-        $team->setAttribute('tournament_id', $tournament->id);
+        $team->setAttribute('tournament_id', $tournament->first()->id);
         $team->setAttribute('name', $request->input('team-name'));
         $team->save();
 
@@ -43,20 +51,46 @@ class LolTeamSignUpMiddleware
         $captain->setAttribute('phone', $request->input('team-captain-phone'));
         $captain->setAttribute('team_id', $team->id);
         $captain->save();
-        
+
         // add captain to team
         $team->captain = $captain->id;
         $team->save();
 
         // add other players
-        for ($i = 1; $i <= 4; $i++) {
-            $player = new Player();
-            $player->setAttribute('username', $request->input('teammate-'.Numbers::toWord($i).'-lol-summoner-name'));
-            $player->setAttribute('email', $request->input('teammate-'.Numbers::toWord($i).'-email-address'));
-            $player->setAttribute('team_id', $team->id);
-            $player->save();
+        for ($i = 1; $i <= config('championship.players.league-of-legends', 5); $i++) {
+            if ($request->input('teammate-' . Numbers::toWord($i) . '-lol-summoner-name')) {
+                $player = new Player();
+                $player->setAttribute(
+                    'username',
+                    $request->input('teammate-' . Numbers::toWord($i) . '-lol-summoner-name')
+                );
+                $player->setAttribute(
+                    'email',
+                    $request->input('teammate-' . Numbers::toWord($i) . '-email-address')
+                );
+                $player->setAttribute('team_id', $team->id);
+                $player->save();
+            }
         }
- 
+
         return $next($request);
+    }
+
+    /**
+     * @param mixed $tournament
+     * @return LolTeamSignUpMiddleware
+     */
+    public function setTournament($tournament)
+    {
+        $this->tournament = $tournament;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTournament()
+    {
+        return $this->tournament;
     }
 }
