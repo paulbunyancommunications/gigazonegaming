b768216d81cb802c6ef251a622c5976940599701
Nate Nolting <me@natenolting.com>
2016-07-07 08:50:37 -0500

    moved context files to sub directory under theme and updated loader got context

---
+++ public_html/wp-content/themes/gigazone-gaming/context/rollbar.php
@@ -0,0 +1,3 @@
+<?php
+// token for client side error reporting
+$context['ROLLBAR_CLIENT_SIDE_TOKEN'] = getenv('ROLLBAR_CLIENT_SIDE_TOKEN');
--- public_html/wp-content/themes/gigazone-gaming/get-context.php
+++ public_html/wp-content/themes/gigazone-gaming/get-context.php
@@ -1,6 +1,6 @@
 <?php
-$contextFiles = glob(__DIR__ . '/context-*.php');
+$contextFiles = glob(__DIR__ . '/context/*.php');
 $context = [];
-for($i=0; $i < count($contextFiles); $i++) {
-    include(locate_template(pathinfo($contextFiles[$i], PATHINFO_BASENAME)));
+for ($i = 0; $i < count($contextFiles); $i++) {
+    include $contextFiles[$i];
 };
