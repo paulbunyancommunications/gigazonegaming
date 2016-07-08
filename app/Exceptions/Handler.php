10a249287845c55970013856f269cb43aa4e8c47
Nate Nolting <me@natenolting.com>
2016-07-08 10:55:15 -0500

    Changed logging warnings to errors for Rollbar to pickup.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,7 +34,7 @@ class Handler extends ExceptionHandler
     public function report(Exception $e)
     {
         // only log errors to Rollbar if in production
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') == 'production') {
             \Log::error($e);
         }
         parent::report($e);
