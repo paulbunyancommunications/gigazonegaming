ace233f94ccf9fdf4195391ddbe0f119aaa292b3
Nate Nolting <me@natenolting.com>
2016-08-09 16:58:01 -0500

    added comment template placeholder and reorganized post type partial templates

---
+++ public_html/wp-content/themes/gigazone-gaming/comments.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * ${CLASS_NAME}
+ *
+ * Created 8/9/16 4:46 PM
+ * Description of this file here....
+ *
+ * @author Nate Nolting <naten@paulbunyan.net>
+ * @package ${NAMESPACE}
+ * @subpackage Subpackage
+ */
