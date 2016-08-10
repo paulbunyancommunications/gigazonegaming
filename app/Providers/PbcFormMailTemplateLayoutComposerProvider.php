96bbad3443213d9ce0d1c772f458d30fa15786a4
Nate Nolting <me@natenolting.com>
2016-08-10 08:01:07 -0500

    added check to make sure links don't get duplicated when adding to view data.

--- app/Providers/PbcFormMailTemplateLayoutComposerProvider.php
+++ app/Providers/PbcFormMailTemplateLayoutComposerProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\WpLink;
 use Cocur\Slugify\Slugify;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,16 +18,23 @@ class PbcFormMailTemplateLayoutComposerProvider extends ServiceProvider
         \View::composer(['partials.form_mail.social_media_icons'], function ($view) {
 
             $slugify = new Slugify();
-            $links = \App\Models\WpLink::whereIn(
+            $links = WpLink::whereIn(
                 'link_name',
                 ['Facebook', 'Twitter', 'Instagram', 'Youtube', 'Twitch']
             )
                 ->get();
             $social = [];
+            // holder for checking if this link has already been added
+            $found = [];
             if ($links) {
                 foreach ($links as $key => $link) {
+                    // check if we've already added this link.
+                    if (in_array($social[$key]['link_name'], $found)) {
+                        continue;
+                    }
+                    array_push($found, $social[$key]['link_name']);
                     $social[$key] = $link->toArray();
-                    $social[$key]['link_image'] = 'https://gigazonegaming.com/wp-content/uploads/2016/06/'.strtolower($social[$key]['link_name']).'.png';
+                    $social[$key]['link_image'] = 'https://gigazonegaming.com/wp-content/uploads/2016/06/' . strtolower($social[$key]['link_name']) . '.png';
                     $social[$key]['link_slug'] = $slugify->slugify($social[$key]['link_name']);
 
                     // make base64 image version of image
