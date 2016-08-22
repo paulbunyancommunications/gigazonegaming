45421d293ed7c41eebf82058ee062eb3805f234d
Nate Nolting <me@natenolting.com>
2016-08-22 16:13:50 -0500

    updated photo rotator so that the nav button doesn't overlap the link on the image

--- public_html/wp-content/themes/gigazone-gaming/context/photo-rotator.php
+++ public_html/wp-content/themes/gigazone-gaming/context/photo-rotator.php
@@ -12,11 +12,11 @@ if (function_exists('get_attachments_by_media_tags')
 ) {
     $rotatorImages = get_attachments_by_media_tags('media_tags=' . $context['page']->slug . '-masthead-photo-rotator&media_types=jpeg,jpg,gif,png&orderby=post_title&order=DESC');
     if ($rotatorImages) {
+
         foreach ($rotatorImages as $key => $img) {
             $imgPath = substr($img->guid, strlen($context['http_host']), strlen($img->guid));
             $rotatorImages[$key]->imager = '/bower_components/image.php/image.php/' . $img->ID . '.jpg?width={width}&amp;image=' . $imgPath;
         }
-
         $context['masthead_photo_rotator'] = $rotatorImages;
     }
 }
