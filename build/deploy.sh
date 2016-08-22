93966f745b44743230f3193f94278e538f9631e0
Nate Nolting <me@natenolting.com>
2016-08-22 11:05:19 -0500

    added halting to deploy script if at any point it fails

--- build/deploy.sh
+++ build/deploy.sh
@@ -6,5 +6,27 @@
 # eg: "bash deploy.sh development 1" will run deployment in development mode with metrics
 env=$1
 metrics=$2
-phing build:deploy -Denv ${env} -Ddo_metrics ${metrics}
-phing build:post_deploy -Denv ${env}
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#http://stackoverflow.com/a/8217870
+safeRunCommand() {
+  typeset cmnd="$*"
+  typeset ret_code
+
+  echo cmnd=$cmnd
+  eval $cmnd
+  ret_code=$?
+  if [ $ret_code != 0 ]; then
+    printf "Error : [%d] when executing command: '$cmnd'" $ret_code
+    exit $ret_code
+  fi
+}
+
+
+deployment="phing build:deploy -Denv ${env} -Ddo_metrics ${metrics}"
+cleanup="phing build:post_deploy -Denv ${env}"
+push="cd ..; bash push.sh; cd ${dir}"
+
+safeRunCommand "$deployment"
+safeRunCommand "$cleanup"
+safeRunCommand "$push"
