55a8c5e38ff6f92220285f07281747375dd58e2c
Nate Nolting <me@natenolting.com>
2016-08-23 10:11:53 -0500

    fixed parsing of image path

--- public_html/wp-content/themes/gigazone-gaming/context/photo-rotator.php
+++ public_html/wp-content/themes/gigazone-gaming/context/photo-rotator.php
@@ -14,7 +14,7 @@ if (function_exists('get_attachments_by_media_tags')
     if ($rotatorImages) {
 
         foreach ($rotatorImages as $key => $img) {
-            $imgPath = substr($img->guid, strlen($context['http_host']), strlen($img->guid));
+            $imgPath = parse_url($img->guid, PHP_URL_PATH);
             $rotatorImages[$key]->imager = '/bower_components/image.php/image.php/' . $img->ID . '.jpg?width={width}&amp;image=' . $imgPath;
         }
         $context['masthead_photo_rotator'] = $rotatorImages;
