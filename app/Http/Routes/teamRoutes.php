0a304425bc0adee8d73dde503a7c3272c6332f6e
Nelson <manklar@hotmail.com>
2016-08-29 17:31:32 -0500

    Team route updates

--- app/Http/Routes/teamRoutes.php
+++ app/Http/Routes/teamRoutes.php
@@ -12,5 +12,6 @@ Route::group(['middleware' => ['WPAdmin']], function () {
     Route::get('/manage/team/edit/{team_id}', ['as' => 'manage.team.edit', 'uses' => 'Backend\Manage\TeamsController@edit']);
     Route::put('/manage/team/edit/{team_id}', ['as' => 'manage.team.edited', 'uses' => 'Backend\Manage\TeamsController@update']);
     Route::post('/manage/team/new/', ['as' => 'manage.team.new', 'uses' => 'Backend\Manage\TeamsController@create']);
-    Route::delete('/manage/team/delete/{team_id}', ['as' => 'manage.team.delete', 'uses' => 'Backend\Manage\TeamsController@destroy']);
+    Route::delete('/manage/team/delete_soft/{team_id}', ['as' => 'manage.team.delete_soft', 'uses' => 'Backend\Manage\TeamsController@destroy_soft']);
+    Route::delete('/manage/team/delete_hard/{team_id}', ['as' => 'manage.team.delete_hard', 'uses' => 'Backend\Manage\TeamsController@destroy_hard']);
 });
