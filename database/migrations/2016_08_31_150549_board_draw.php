471954e69452d8ec5425b54d96dfe50d23536187
Nelson <manklar@hotmail.com>
2016-08-31 12:33:33 -0500

    Ignore db files

---
+++ database/migrations/2016_08_31_150549_board_draw.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BoardDraw extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::connection('mysql_champ')->hasTable('games_players')) {
+            Schema::connection('mysql_champ')->create('games_players', function (Blueprint $table) { //pivot table
+                $table->engine = "InnoDB";
+                $table->increments('id');
+                $table->string('players_id')->references('id')->on('players');
+                $table->string('tournament_id')->references('id')->on('tournaments');
+                $table->timestamps();
+            });
+        }
+        if(!Schema::connection('mysql_champ')->hasTable('players')) {
+            Schema::connection('mysql_champ')->table('players', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->integer('user_id')->unsigned()->after('id');
+                $table->foreign('user_id')->references('id')->on('users');
+            });
+        }
+
+
+        //now straighting out foreign keys and references
+        if(Schema::connection('mysql_champ')->hasColumn('tournaments', 'game_id')) {
+            Schema::connection('mysql_champ')->table('tournaments', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->integer('game_id')->unsigned()->after('max_players')->change();
+                $table->foreign('game_id')->references('id')->on('games');
+            });
+        }
+
+        if(Schema::connection('mysql_champ')->hasColumn('teams', 'tournament_id')) {
+            Schema::connection('mysql_champ')->table('teams', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->integer('tournament_id')->unsigned()->after('id')->change();
+                $table->foreign('tournament_id')->references('id')->on('tournament');
+            });
+        }
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if(Schema::connection('mysql_champ')->hasTable('games_players')) {
+            Schema::connection('mysql_champ')->drop('games_players');
+        }
+
+        if(Schema::connection('mysql_champ')->hasColumn('players','user_id')) {
+            Schema::connection('mysql_champ')->table('players', function (Blueprint $table) {
+                $table->dropForeign('user_id');
+                $table->dropColumn('user_id');
+            });
+        }
+        if(Schema::connection('mysql_champ')->hasColumn('tournaments','game_id')) {
+            Schema::connection('mysql_champ')->table('tournaments', function (Blueprint $table) {
+                $table->dropForeign('game_id');
+                $table->dropColumn('game_id');
+            });
+        }
+        if(Schema::connection('mysql_champ')->hasColumn('teams','tournament_id')) {
+            Schema::connection('mysql_champ')->table('teams', function (Blueprint $table) {
+                $table->dropForeign('tournament_id');
+                $table->dropColumn('tournament_id');
+            });
+        }
+
+    }
+}
