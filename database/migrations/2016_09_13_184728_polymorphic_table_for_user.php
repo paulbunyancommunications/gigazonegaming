afe4e8adc2cb5fbb81623f437f0489991053963e
Nate Nolting <me@natenolting.com>
2016-09-14 13:31:37 -0500

    removed reference to old table in polymorph migration

--- database/migrations/2016_09_13_184728_polymorphic_table_for_user.php
+++ database/migrations/2016_09_13_184728_polymorphic_table_for_user.php
@@ -21,34 +21,6 @@ class PolymorphicTableForUser extends Migration
                 $table->string("relation_type");
                 $table->timestamps();
             });
-//            $allPlayers = \App\Models\Championship\Player::
-            $allPlayers = json_decode(json_encode(DB::connection('mysql_champ')->table('game_player_team_tournament')->get()),TRUE);
-
-            foreach ($allPlayers as $key => $player) {
-                if (isset($player['team_id']) and $player['team_id'] != '' and $player['team_id'] != [] and $player['team_id'] != null) {
-                    DB::connection('mysql_champ')->table('player_relations')
-                        ->insert([
-                            'relation_type' => 'App\Models\Championship\Team',
-                            'relation_id' => $player['team_id'],
-                            "player_id" => $player['player_id']
-                        ]);
-                }
-                DB::connection('mysql_champ')
-                    ->table('player_relations')
-                    ->insert([
-                            'relation_type' => 'App\Models\Championship\Game',
-                            'relation_id' => $player['game_id'],
-                            "player_id" => $player['player_id']
-                        ]);
-                DB::connection('mysql_champ')
-                    ->table('player_relations')
-                    ->insert(
-                        [
-                            'relation_type' => 'App\Models\Championship\Tournament',
-                            'relation_id' => $player['tournament_id'],
-                            "player_id" => $player['player_id']
-                        ]);
-            }
         }
     }
 
