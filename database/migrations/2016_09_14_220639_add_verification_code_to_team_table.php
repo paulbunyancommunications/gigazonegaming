9743a21f9ab1915763a03348845177dae10351c2
Nelson <manklar@hotmail.com>
2016-09-14 17:16:12 -0500

    Another migration. This time verification code(token) for the teams

---
+++ database/migrations/2016_09_14_220639_add_verification_code_to_team_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVerificationCodeToTeamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::connection('mysql_champ')->hasColumn('teams', 'verification_code')) {
+            Schema::connection('mysql_champ')->table('teams', function (Blueprint $table) {
+                $table->engine = 'InnoDB';
+                $table->string('verification_code')->default("aaaaaa")->after('name');
+            });
+        }
+        $teamUpdated = \App\Models\Championship\Team::all()->toArray();
+        foreach ($teamUpdated as $team){
+            \App\Models\Championship\Team::where('id','=',$team['id'])->update(['verification_code'=> str_random(6)]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::connection('mysql_champ')->hasColumn('teams','verification_code')) {
+            Schema::connection('mysql_champ')->table('teams', function (Blueprint $table) {
+                $table->dropColumn('verification_code');
+            });
+        }
+    }
+}
