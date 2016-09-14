3d4113a98104899a8b7577229e988ea7a596e0b1
Nelson <manklar@hotmail.com>
2016-09-14 14:53:17 -0500

    more migrations. All information moved to the player_relations (polymorphic table)

--- database/migrations/2016_09_13_184728_polymorphic_table_for_players.php
+++ database/migrations/2016_09_13_184728_polymorphic_table_for_players.php
@@ -20,36 +20,8 @@ class PolymorphicTableForPlayers extends Migration
                 $table->integer("relation_id");
                 $table->string("relation_type");
                 $table->timestamps();
+                $table->unique(['player_id', 'relation_id', 'relation_type']);
             });
-//            $allPlayers = \App\Models\Championship\Player::
-            $allPlayers = json_decode(json_encode(DB::connection('mysql_champ')->table('game_player_team_tournament')->get()),TRUE);
-
-            foreach ($allPlayers as $key => $player) {
-                if (isset($player['team_id']) and $player['team_id'] != '' and $player['team_id'] != [] and $player['team_id'] != null) {
-                    DB::connection('mysql_champ')->table('player_relations')
-                        ->insert([
-                            'relation_type' => 'App\Models\Championship\Team',
-                            'relation_id' => $player['team_id'],
-                            "player_id" => $player['player_id']
-                        ]);
-                }
-                //we wont care about game relations
-//                DB::connection('mysql_champ')
-//                    ->table('player_relations')
-//                    ->insert([
-//                            'relation_type' => 'App\Models\Championship\Game',
-//                            'relation_id' => $player['game_id'],
-//                            "player_id" => $player['player_id']
-//                        ]);
-                DB::connection('mysql_champ')
-                    ->table('player_relations')
-                    ->insert(
-                        [
-                            'relation_type' => 'App\Models\Championship\Tournament',
-                            'relation_id' => $player['tournament_id'],
-                            "player_id" => $player['player_id']
-                        ]);
-            }
         }
     }
 
---
+++ database/migrations/2016_09_14_184246_move_individual_players_to_polymorphic_table_for_players.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MoveIndividualPlayersToPolymorphicTableForPlayers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //we only have a game and as every single player has the game id of 2 we suppose that
+        // the player already signed up for the lol tournament with an ID of 1
+        // (THERE ARE NO OTHER TOURNAMENTS AT THIS TIME!)
+        $allPlayers = \App\Models\Championship\Player::all()->toArray();
+        foreach ($allPlayers as $key => $player) {
+            unset($player['game_id']);
+            $player['team_id'] = null;
+            $newPlayer = \App\Models\Championship\Player::create($player);
+            $relation = \App\Models\Championship\PlayerRelation::create(
+                    [
+                        'relation_type' => 'App\Models\Championship\Tournament',
+                        'relation_id' => 1,
+                        "player_id" => $newPlayer->id
+                    ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // we don't need one as we would still, as it is right now,
+        // have all the info added to the polymorphic table in their
+        // respective tables.
+    }
+}
