ca6a97caab62e1e7b0164af3acb9138dba0de8df
Nate Nolting <me@natenolting.com>
2016-09-19 16:36:52 -0500

    added fix to single.php where there needs to be a check if there are any tags before getting the average

--- public_html/wp-content/themes/gigazone-gaming/single.php
+++ public_html/wp-content/themes/gigazone-gaming/single.php
@@ -18,7 +18,7 @@ $context['single'] = true;
 // find related posts that are of a similar tag and the tag is higher than the average
 $termIds = Arrays::pluck($context['post']->terms, 'term_id');
 $terms = get_terms('post_tag', ['include' => $termIds]);
-$average = Arrays::average(Arrays::pluck($terms, 'count'));
+$average = count($terms) ? Arrays::average(Arrays::pluck($terms, 'count')) : 0;
 $lessThanAverageTags = Arrays::filter($terms, function ($value) use ($average) {
     return $value->count <= $average;
 });
