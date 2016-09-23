123016e9b366393d94acf21676e129f22080d0bb
Nelson <manklar@hotmail.com>
2016-09-23 15:23:31 -0500

    making the teamMaker view prettier :)

--- resources/views/game/teamMaker.blade.php
+++ resources/views/game/teamMaker.blade.php
@@ -2,11 +2,20 @@
 @extends('game.base')
 
 @section('css')
+    .grouper_team_maker{
+        border:5px solid #000;
+        background: #c9c9c9;
+        padding:30px 10px 10px 10px;
+    }
+    #team_selected h1{
+        border-bottom:5px solid #000;
+        padding-bottom:5px;
+    }
 @endsection
 @section('content')
 
-    <label for="game_sort" style="width:180px; text-align:right;">Pick a Game :</label>
-    <select name="game_sort" id="game_sort" style="width:350px; text-align:left;">
+    <label for="game_sort" style="text-align:right;" class="col-sm-12">Pick a Game :</label>
+    <select name="game_sort" id="game_sort" style="text-align:left;" class="col-sm-12">
 
         <option> --- </option>
         @foreach($games as $g)
@@ -16,8 +25,8 @@
         @endforeach
     </select>
     <br />
-    <label for="tournament_sort" style="width:180px; text-align:right;">Tournament: </label>
-    <select name="tournament_sort" id="tournament_sort" style="width:350px; text-align:left;">
+    <label for="tournament_sort" style="text-align:right;" class="col-sm-12">Tournament: </label>
+    <select name="tournament_sort" id="tournament_sort" style="text-align:left;" class="col-sm-12">
         <option> --- </option>
         @foreach($tournaments as $g)
             <option id="t_option{{$g['game_id']}}_{{$g['tournament_id']}}"  max_players="{{$g['max_players']}}" value="{{$g['tournament_id']}}" class="fa"
@@ -25,10 +34,10 @@
             >{{$g['tournament_name']}} ({{$g['max_players']}} players)</option>
         @endforeach
     </select>
-    <input type="submit" value="Create Team for Selected Tournament"  id="team_creator_btn" class="btn btn-primary">
+    <input type="submit" value="Create Team for Selected Tournament"  id="team_creator_btn" class="btn btn-primary col-sm-12 col-md-4">
     <br />
-    <label for="team_sort" style="width:180px; text-align:right;">Filter by Team: </label>
-    <select name="team_sort" id="team_sort" style="width:350px; text-align:left;">
+    <label for="team_sort" style="text-align:right;" class="col-sm-12">Fill Team: </label>
+    <select name="team_sort" id="team_sort" style="text-align:left;" class="col-sm-12">
         <option> --- </option>
         @foreach($teams as $g)
             @if($g['team_max_players'] > $g['team_count'])
@@ -38,17 +47,21 @@
             @endif
         @endforeach
     </select>
-    <input type="submit" value="Fill selected Team" id="team_fill_btn" class="btn btn-primary">
+    <input type="submit" value="Fill selected Team" id="team_fill_btn" class="btn btn-primary col-sm-12 col-md-4">
     <br />
     <div id="team_selected">
         {{ Form::open(array('id' => "teamFiller", 'action' => array('Backend\Manage\IndividualPlayersController@teamFill'))) }}
         <input name="_method" type="hidden" value="PUT">
+        <h1 id="title_f" class='col-sm-12 hidden'>Add These Players To The Selected Team</h1>
         <div id="teamFilling"></div>
         <div id='submit_fill_team' class='btn btn-danger col-sm-12 hidden'>Save Team</div>
         {{ Form::close() }}
         {{ Form::open(array('id' => "teamCreator", 'action' => array('Backend\Manage\IndividualPlayersController@teamCreate'))) }}
         <input name="_method" type="hidden" value="POST">
+        <h1 id="title_c" class='col-sm-12 hidden'>Add These Players To A New Team</h1>
         <div id="teamCreating"></div>
+        <div id='submit_create_team' class='btn btn-danger col-sm-12 hidden'>Create Team</div>
+
         {{ Form::close() }}
     </div>
     <br />
