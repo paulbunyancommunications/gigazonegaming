871efb6cf7065fc65440e1ee56ae93686da59388
Nate Nolting <me@natenolting.com>
2016-09-27 16:06:31 -0500

    fixed auth namespace in routes and removed user routes

--- app/Http/Routes/Auth.php
+++ app/Http/Routes/Auth.php
@@ -1,21 +1,21 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Auth'], function()
+Route::group(['middleware' => 'web', 'prefix' => 'auth', 'namespace' => 'Auth'], function()
 {
 	Route::get('/', 'AuthController@index');
 });
 
-Route::group(['middleware' => 'guest'], function () {
+Route::group(['middleware' => 'guest', 'namespace' => 'Auth'], function () {
 
     Route::any('/auth', function () {
         return redirect('/auth/login')->with('warning', trans('auth.must_login'));
     });
-    Route::get('/auth/login', ['as' => 'login', 'uses' => 'Auth\AuthController@login']);
-    Route::post('/auth/login', ['as' => 'authenticate', 'uses' => 'Auth\AuthController@authenticate']);
-    Route::get('/auth/create', ['as' => 'create', 'uses' => 'Auth\AuthController@create']);
-    Route::post('/auth/create', ['as' => 'generate', 'uses' => 'Auth\AuthController@generate']);
-    Route::get('/auth/recover', 'Auth\PasswordController@recover');
-    Route::post('/auth/recover', 'Auth\PasswordController@sendRecoverCode');
-    Route::get('/auth/reset', 'Auth\PasswordController@reset');
-    Route::put('/auth/reset/{user}', 'Auth\PasswordController@resetPassword');
+    Route::get('/auth/login', ['as' => 'login', 'uses' => 'AuthController@login']);
+    Route::post('/auth/login', ['as' => 'authenticate', 'uses' => 'AuthController@authenticate']);
+    Route::get('/auth/create', ['as' => 'create', 'uses' => 'AuthController@create']);
+    Route::post('/auth/create', ['as' => 'generate', 'uses' => 'AuthController@generate']);
+    Route::get('/auth/recover', 'PasswordController@recover');
+    Route::post('/auth/recover', 'PasswordController@sendRecoverCode');
+    Route::get('/auth/reset', 'PasswordController@reset');
+    Route::put('/auth/reset/{user}', 'PasswordController@resetPassword');
 });
