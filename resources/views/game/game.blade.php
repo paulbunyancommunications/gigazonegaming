b8da7d88b44231bc407d251157c10bc0cc34d1cb
Nate Nolting <me@natenolting.com>
2016-09-28 09:40:11 -0500

    moved to two column layout for games index and fixed for label on title field

--- resources/views/game/game.blade.php
+++ resources/views/game/game.blade.php
@@ -11,7 +11,7 @@
     }
 @endsection
 @section('content')
-
+    <div class="col-md-6">
     @if(isset($theGame->title))
         <h1>Update game &#8220;{{ $theGame->title }}&#8221;</h1>
         {{ Form::open(array('id' => "gameForm", 'action' => array('Backend\Manage\GamesController@update', $theGame->id), 'class' => 'form-horizontal')) }}
@@ -33,7 +33,7 @@
             </div>
         </div>
         <div class="form-group">
-            <label for="name" class="control-label col-xs-3">Game Title:</label>
+            <label for="title" class="control-label col-xs-3">Game Title:</label>
             <div class="col-xs-9">
                 <input type="text" name="title" id="title" class="form-control" placeholder="The title of the game" value="@if(isset($theGame->title)){{ $theGame->title }}@else{{ old('title') }}@endif"/>
             </div>
@@ -56,39 +56,42 @@
             {{ Html::link('/manage/game/', (isset($theGame) ? 'Create a new game' : 'Clear'), array('id' => 'reset', 'class' => 'btn btn-default'))}}
         </div>
     </div>
-    </form>
-    <h2>Game List</h2>
-    <div id="listOfGames" class="listing">
-        @if(!isset($games) || $games == [])
-            <p>There are no games yet</p>
-        @else
-            @foreach($games as $id => $game)
-                <div class="margin-sm-bottom">
-                    {{ Form::open(array('id' => "toForm".$game["game_id"], 'action' => array('Backend\Manage\TournamentsController@filter'), 'class' => "toForms form")) }}
-                    <input name="_method" type="hidden" value="POST">
-                    <input name="game_sort" type="hidden" value="{{$game["game_id"]}}">
-                    {!!
-                        Form::submit(
-                            ($game["game_title"] ? $game["game_title"] : $game["game_name"]),
-                            array('class'=>'gameName btn btn-default btn-block list')
-                        )
-                    !!}
-                    {{ Form::close() }}
-                    &nbsp;&nbsp;
-                    {{ Html::linkAction('Backend\Manage\GamesController@edit', '', array('game_id'=>$game["game_id"]), array('class' => 'btn btn-success list fa fa-pencil-square-o', 'id' => 'edit-'.$game["game_name"], 'title'=>"Edit")) }}
-                    &nbsp;&nbsp;
-                    {{ Form::open(array('id' => "gameForm".$game["game_id"], 'action' => array('Backend\Manage\GamesController@destroy', $game["game_id"]), 'class' => "deletingForms")) }}
-                    <input name="_method" type="hidden" value="DELETE">
-                    {!!
-                        Form::submit(
-                            '&#xf014; &#xf1c0;',
-                            array('class'=>'btn btn-danger list fa fa-times confirm-choice', 'title'=>"Delete From Database", 'id' => 'delete-'.$game["game_name"])
-                        )
-                    !!}
-                    {{ Form::close() }}
-                </div>
-            @endforeach
-        @endif
+    {{ Form::close() }}
+    </div>
+    <div class="col-md-6">
+        <h2>Game List</h2>
+        <div id="listOfGames" class="listing">
+            @if(!isset($games) || $games == [])
+                <p>There are no games yet</p>
+            @else
+                @foreach($games as $id => $game)
+                    <div class="margin-sm-bottom">
+                        {{ Form::open(array('id' => "toForm".$game["game_id"], 'action' => array('Backend\Manage\TournamentsController@filter'), 'class' => "toForms form")) }}
+                        <input name="_method" type="hidden" value="POST">
+                        <input name="game_sort" type="hidden" value="{{$game["game_id"]}}">
+                        {!!
+                            Form::submit(
+                                ($game["game_title"] ? $game["game_title"] : $game["game_name"]),
+                                array('class'=>'gameName btn btn-default btn-block list')
+                            )
+                        !!}
+                        {{ Form::close() }}
+                        &nbsp;&nbsp;
+                        {{ Html::linkAction('Backend\Manage\GamesController@edit', '', array('game_id'=>$game["game_id"]), array('class' => 'btn btn-success list fa fa-pencil-square-o', 'id' => 'edit-'.$game["game_name"], 'title'=>"Edit")) }}
+                        &nbsp;&nbsp;
+                        {{ Form::open(array('id' => "gameForm".$game["game_id"], 'action' => array('Backend\Manage\GamesController@destroy', $game["game_id"]), 'class' => "deletingForms")) }}
+                        <input name="_method" type="hidden" value="DELETE">
+                        {!!
+                            Form::submit(
+                                '&#xf014; &#xf1c0;',
+                                array('class'=>'btn btn-danger list fa fa-times confirm-choice', 'title'=>"Delete From Database", 'id' => 'delete-'.$game["game_name"])
+                            )
+                        !!}
+                        {{ Form::close() }}
+                    </div>
+                @endforeach
+            @endif
+        </div>
     </div>
 
 @endsection
