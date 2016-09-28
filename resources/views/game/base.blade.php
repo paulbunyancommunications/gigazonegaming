a9ee923d3e5a6a71d021564ba6910edab02cb2f5
Nelson <manklar@hotmail.com>
2016-09-28 13:26:55 -0500

    Base view update

--- resources/views/game/base.blade.php
+++ resources/views/game/base.blade.php
@@ -77,18 +77,6 @@
     </div>
     <div class="row">
         <div class="col-md-12">
-            @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            @if(isset($success) and  $success!='')
-                <div class="alert alert-success"><strong>Success!</strong> {{$success}}</div>
-            @endif
             @yield('content')
         </div>
     </div>
