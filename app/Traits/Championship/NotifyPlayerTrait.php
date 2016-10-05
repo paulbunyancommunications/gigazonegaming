29dd5bf7521903472125084fa4cbf5440c3f4749
Nate Nolting <me@natenolting.com>
2016-10-05 13:27:16 -0500

    updated notify player trait with correct information to pass to message

--- app/Traits/Championship/NotifyPlayerTrait.php
+++ app/Traits/Championship/NotifyPlayerTrait.php
@@ -11,42 +11,64 @@ namespace App\Traits\Championship;
  * @package App\Traits\Championship
  */
 
-use App\Http\Requests\IndividualPlayerRequest;
+use App\Http\Requests\PlayerRequest;
+use Carbon\Carbon;
 use Pbc\Bandolier\Type\Strings;
 use Pbc\FormMail\Http\Controllers\FormMailController;
-use Pbc\FormMail\Traits\QueueTrait;
-use Pbc\FormMail\Traits\SendTrait;
-use Pbc\FormMail\Traits\MessageTrait;
 use Pbc\FormMail\Helpers\FormMailHelper;
 use App\Models\Championship\Player;
 use Pbc\Premailer;
 
 trait NotifyPlayerTrait
 {
-    use QueueTrait, SendTrait, MessageTrait;
 
     protected $premailer;
     protected $helper;
 
-    public function notifyPlayerCreated(IndividualPlayerRequest $request, Player $player)
+    /**
+     * @param PlayerRequest $request
+     * @param Player $player
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function notifyPlayerCreated(PlayerRequest $request, Player $player)
     {
         $premailer = new Premailer();
         $helper = new FormMailHelper();
+        $handler = new FormMailController($premailer, $helper);
+
         $fields = $request->all();
         $fields['fields'] = [];
         foreach(array_keys($fields) as $field) {
-            if(in_array($field, ['_method','_token','fields'])) {
+            if(in_array($field, ['_method','_token','fields','submit'])) {
                 continue;
             }
             array_push($fields['fields'], $field);
             $fields[$field.'-label'] = Strings::formatForTitle($field);
         }
         $request->replace($fields);
-        $handler = new FormMailController($premailer, $helper);
-        $data = [];
-        $handler->requestHandler($request, $data);
 
-
+        $hostUrl = parse_url(\Config::get('app.url'), PHP_URL_HOST);
+        $paramsForLang = array_merge(
+            $player->toArray(),
+            [
+                'url' => $hostUrl,
+                'form' => '',
+                'time' => Carbon::now(),
+            ]
+        );
+        $data = [
+            'subject' => json_encode([
+                $handler::RECIPIENT => \Lang::get($helper::resourceRoot() .'.'. \Route::currentRouteName() . '.subject.'.$handler::RECIPIENT, $paramsForLang),
+                $handler::SENDER => \Lang::get($helper::resourceRoot() .'.'. \Route::currentRouteName() . '.subject.'.$handler::SENDER, $paramsForLang),
+            ]),
+            $handler::SENDER => 'players@'.$hostUrl,
+            $handler::RECIPIENT => $request->input('email'),
+            'head' => json_encode([
+                $handler::RECIPIENT => \Lang::get($helper::resourceRoot() .'.'. \Route::currentRouteName() . '.'.$handler::RECIPIENT, $paramsForLang),
+                $handler::SENDER => \Lang::get($helper::resourceRoot() .'.'. \Route::currentRouteName() . '.'.$handler::SENDER, $paramsForLang),
+            ]),
+        ];
+        return $handler->requestHandler($request, $data);
     }
 }
 
