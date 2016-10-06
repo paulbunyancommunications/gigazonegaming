8df261331f99c006f6a319bc508fecc979048847
Nelson <manklar@hotmail.com>
2016-10-06 10:38:52 -0500

    Adding new helper class for testing :)

---
+++ tests/acceptance/App/Http/Controllers/Backend/Manage/CreatorHelper.php
@@ -0,0 +1,238 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nelson_castillo
+ * Date: 10/5/16
+ * Time: 14:41
+ */
+
+namespace Tests\Acceptance\App\Http\Controllers\Backend\Manage;
+
+use \AcceptanceTester;
+use \BaseAcceptance;
+use App\Models\Championship\Tournament;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Mockery;
+
+/**
+ * Class PlayersControllerCest
+ * @package Tests\Acceptance\App\Http\Controllers\Backend\Manage
+ */
+class CreatorHelper extends BaseAcceptance
+{
+    private $pl_name;
+    private $pl_email;
+    private $pl_phone;
+    private $pl_username;
+    private $ga_name;
+    private $ga_title;
+    private $to_name;
+    private $te_name;
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getAGame(AcceptanceTester $I){
+        $this->goToViewAndCreateAGame($I);
+        return $this->ga_name;
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateAGame(AcceptanceTester $I)
+    {
+        $this->ga_name = $this->faker->name;
+        $this->ga_title = $this->faker->name;
+        $I->amOnPage('/app/manage/game');
+        $I->fillField('name', $this->ga_name);
+        $I->fillField('title', $this->ga_title);
+        $I->fillField('uri', $this->faker->url);
+        $I->fillField('description', $this->faker->words(5));
+        $I->click('submit');
+        $I->see("The game " . $this->ga_title . " was added!");
+
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getATournament(AcceptanceTester $I){
+        $this->goToViewAndCreateATournament($I);
+        return $this->to_name;
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateATournament(AcceptanceTester $I)
+    {
+        $this->to_name = $this->faker->name;
+        $I->amOnPage('/app/manage/tournament');
+        $I->fillField('name', $this->to_name);
+        $I->fillField('max_players', $this->faker->numberBetween(1, 10));
+        $I->selectOption(['id' => 'game_id'], $this->ga_name);
+        $I->click('submit');
+        $I->see("The tournament " . $this->to_name . " was added");
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getATeam(AcceptanceTester $I){
+        $this->goToViewAndCreateATeam($I);
+        return $this->te_name;
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateATeam(AcceptanceTester $I)
+    {
+        $this->te_name = $this->faker->name;
+        $I->amOnPage('/app/manage/team');
+        $I->fillField('name', $this->te_name);
+        $I->selectOption(['id' => 'tournament_id'], $this->to_name);
+        $I->click('submit');
+        $I->see("The team " . $this->te_name . " was added");
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getAPlayerWithGaToTeAssociations(AcceptanceTester $I){
+        $this->createTeamTournamentGame($I);
+        $this->goToViewAndCreateAPlayerWithGameAndTournamentAndTeamAssociations($I);
+        return [
+            'name'=>$this->pl_name,
+            'username'=>$this->pl_username,
+            'email'=> $this->pl_email,
+            'phone'=> $this->pl_phone,
+            'team'=> $this->te_name,
+            'tournament'=> $this->to_name,
+            'game'=> $this->ga_name,
+        ];
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateAPlayerWithGameAndTournamentAndTeamAssociations(AcceptanceTester $I)
+    {
+        $this->makePlayerFakeInfo();
+        $I->amOnPage('/app/manage/player');
+        $I->fillField('name', $this->pl_name);
+        $I->fillField('username', $this->pl_username);
+        $I->fillField('email', $this->pl_email);
+        $I->fillField('phone', $this->pl_phone);
+        $I->selectOption(['id' => 'team_id'], $this->te_name);
+        $I->selectOption(['id' => 'tournament_id'], $this->to_name);
+        $I->selectOption(['id' => 'game_id'], $this->ga_name);
+        $I->click('submit');
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getAPlayerWithGaToAssociations(AcceptanceTester $I){
+        $this->createTeamTournamentGame($I);
+        $this->goToViewAndCreateAPlayerWithGameAndTournamentAssociations($I);
+        return [
+            'name'=>$this->pl_name,
+            'username'=>$this->pl_username,
+            'email'=> $this->pl_email,
+            'phone'=> $this->pl_phone,
+            'team'=> $this->te_name,
+            'tournament'=> $this->to_name,
+            'game'=> $this->ga_name,
+        ];
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateAPlayerWithGameAndTournamentAssociations(AcceptanceTester $I)
+    {
+        $this->makePlayerFakeInfo();
+        $I->amOnPage('/app/manage/player');
+        $I->fillField('name', $this->pl_name);
+        $I->fillField('username', $this->pl_name);
+        $I->fillField('email', $this->pl_email);
+        $I->fillField('phone', $this->pl_phone);
+        $I->selectOption(['id' => 'tournament_id'], $this->to_name);
+        $I->selectOption(['id' => 'game_id'], $this->ga_name);
+        $I->click('submit');
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getAPlayerWithGaAssociations(AcceptanceTester $I){
+        $this->createTeamTournamentGame($I);
+        $this->goToViewAndCreateAPlayerWithGameAssociations($I);
+        return [
+            'name'=>$this->pl_name,
+            'username'=>$this->pl_username,
+            'email'=> $this->pl_email,
+            'phone'=> $this->pl_phone,
+            'team'=> $this->te_name,
+            'tournament'=> $this->to_name,
+            'game'=> $this->ga_name,
+        ];
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateAPlayerWithGameAssociations(AcceptanceTester $I)
+    {
+        $this->makePlayerFakeInfo();
+        $I->amOnPage('/app/manage/player');
+        $I->fillField('name', $this->pl_name);
+        $I->fillField('username', $this->pl_name);
+        $I->fillField('email', $this->pl_email);
+        $I->fillField('phone', $this->pl_phone);
+        $I->selectOption(['id' => 'game_id'], $this->ga_name);
+        $I->click('submit');
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function getAPlayerWithNoAssociations(AcceptanceTester $I){
+        $this->createTeamTournamentGame($I);
+        $this->goToViewAndCreateAPlayerWithoutAssociations($I);
+        return [
+            'name'=>$this->pl_name,
+            'username'=>$this->pl_username,
+            'email'=> $this->pl_email,
+            'phone'=> $this->pl_phone,
+            'team'=> $this->te_name,
+            'tournament'=> $this->to_name,
+            'game'=> $this->ga_name,
+        ];
+    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function goToViewAndCreateAPlayerWithoutAssociations(AcceptanceTester $I)
+    {
+        $this->makePlayerFakeInfo();
+        $I->amOnPage('/app/manage/player');
+        $I->fillField('name', $this->pl_name);
+        $I->fillField('username', $this->pl_name);
+        $I->fillField('email', $this->pl_email);
+        $I->fillField('phone', $this->pl_phone);
+        $I->click('submit');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    private function createTeamTournamentGame(AcceptanceTester $I)
+    {
+        $this->goToViewAndCreateAGame($I);
+        $this->goToViewAndCreateATournament($I);
+        $this->goToViewAndCreateATeam($I);
+    }
+
+    private function makePlayerFakeInfo()
+    {
+        $this->pl_name = $this->faker->name;
+        $this->pl_username = $this->faker->userName;
+        $this->pl_email = $this->faker->email;
+        $this->pl_phone = $this->faker->phoneNumber;
+    }
+}
--- tests/acceptance/App/Http/Controllers/Backend/Manage/PlayersControllerCest.php
+++ tests/acceptance/App/Http/Controllers/Backend/Manage/PlayersControllerCest.php
@@ -8,8 +8,7 @@ use \BaseAcceptance;
  * Class PlayersControllerCest
  * @package Tests\Acceptance\App\Http\Controllers\Backend\Manage
  */
-class PlayersControllerCest extends BaseAcceptance
-{
+class PlayersControllerCest extends BaseAcceptance{
     /**
      * @param AcceptanceTester $I
      */
@@ -42,153 +41,25 @@ class PlayersControllerCest extends BaseAcceptance
         $I->see('Create a new player');
     }
 
-//    /**
-//     * @param AcceptanceTester $I
-//     */
-//    public function tryToCreateAPlayer(AcceptanceTester $I)
-//    {
-//        $I->wantTo('create a player on the management page');
-//        list($name, $max_players, $game_id) = $this->createAPlayer($I);
-//
-//        // check return, we should have a message and all the fields filled
-//        $I->see('The player '.$name.' was added');
-//
-//    }
-//
-//    /**
-//     * @param AcceptanceTester $I
-//     */
-//    public function tryToUpdateAPlayer(AcceptanceTester $I)
-//    {
-//        $I->wantTo('update a player on the management page');
-//
-//        list($name, $max_players, $game_id) = $this->createAPlayer($I);
-//
-//        $name2 = implode('-', $this->faker->words(3));
-//        $max_players2 = $this->faker->numberBetween(1, 10);
-//        $I->click(['id'=>'edit-'.$name]);
-//        $I->seeInField(['id' => 'name'], $name);
-//        $I->seeInField(['id' => 'max_players'], $max_players);
-//        $I->seeOptionIsSelected(['id' => 'game_id'], $game_id);
-//        // fill fields with new values
-//        $I->fillField(['id' => 'name'], $name2);
-//        $I->fillField(['id' => 'max_players'], $max_players2);
-//        $I->selectOption(['id' => 'game_id'], $game_id);
-//
-//        $I->click(['id' => 'submit']);
-//
-//        // check that the fields are now all updated
-//        $I->see('The player '. $name2.' was updated');
-//        $I->seeInField(['id' => 'name'], $name2);
-//        $I->seeInField(['id' => 'max_players'], $max_players2);
-//        $I->seeOptionIsSelected(['id' => 'game_id'], $game_id);
-//
-//
-//    }
-//
-//    /**
-//     * @param AcceptanceTester $I
-//     */
-//    public function tryAndUpdateAPlayerKeepingEverythingTheSame(AcceptanceTester $I)
-//    {
-//        $I->wantTo('update a player, keeping the everything the same.');
-//
-//        // make a player, then update
-//        list($name, $max_players, $game_id) = $this->createAPlayer($I);
-//        $I->click(['id'=>'edit-'.$name]);
-//        $I->seeInField(['id' => 'name'], $name);
-//        $I->seeInField(['id' => 'max_players'], $max_players);
-//        $I->seeOptionIsSelected(['id' => 'game_id'], $game_id);
-//        // fill fields with new values
-//        $I->fillField(['id' => 'name'], $name);
-//        $I->fillField(['id' => 'max_players'], $max_players);
-//        $I->selectOption(['id' => 'game_id'], $game_id);
-//
-//        $I->click(['id' => 'submit']);
-//
-//        // check that the fields are now all updated
-//        $I->see('The player '. $name.' was updated');
-//        $I->seeInField(['id' => 'name'], $name);
-//        $I->seeInField(['id' => 'max_players'], $max_players);
-//        $I->seeOptionIsSelected(['id' => 'game_id'], $game_id);
-//
-//    }
-//
-//    /**
-//     * Ensure when creating a player that the name is present
-//     *
-//     * @param AcceptanceTester $I
-//     */
-//    public function seeErrorWhenNameIsMissing(AcceptanceTester $I)
-//    {
-//        $I->click(['id' => 'submit']);
-//        $I->see('The Player Name is required.');
-//    }
-//    /**
-//     * Ensure when creating a player that the name is present
-//     *
-//     * @param AcceptanceTester $I
-//     */
-//    public function seeErrorWhenGameIsntSelected(AcceptanceTester $I)
-//    {
-//        $I->click(['id' => 'submit']);
-//        $I->see('A Game must be selected');
-//    }
-//    /**
-//     * Ensure when creating a player that the name is present
-//     *
-//     * @param AcceptanceTester $I
-//     */
-//    public function seeErrorWhenMaxPlayersIsntInput(AcceptanceTester $I)
-//    {
-//        $I->click(['id' => 'submit']);
-//        $I->see('The Number of players is a required field');
-//    }
-//    /**
-//     * Ensure when creating a player that the name is present
-//     *
-//     * @param AcceptanceTester $I
-//     */
-//    public function seeErrorWhenMaxPlayersIsntANumber(AcceptanceTester $I)
-//    {
-//        $I->fillField(['id' => 'max_players'], "Not A Number");
-//        $I->click(['id' => 'submit']);
-//        $I->see('The Number of players needs to be a number, LOL.');
-//    }
-//
-//    /**
-//     * Ensure when creating a player that the name isn't already being used
-//     *
-//     * @param AcceptanceTester $I
-//     */
-//    public function seeErrorWhenNameIsAlreadyUsed(AcceptanceTester $I)
-//    {
-//        list($name, $max_players, $game_id) = $this->createAPlayer($I);
-//        $I->amOnPage('/app/manage/player');
-//
-//        $I->fillField(['id' => 'name'], $name);
-//        $I->fillField(['id' => 'max_players'], $max_players);
-//        $I->selectOption(['id' => 'game_id'], $game_id);
-//        $I->click(['id' => 'submit']);
-//        $I->see('The name has already been taken');
-//    }
-//
-//
-//    /**
-//     * @param AcceptanceTester $I
-//     * @return array
-//     */
-//    private function createAPlayer(AcceptanceTester $I, $attributes = [])
-//    {
-//        $name = array_key_exists('name', $attributes) ? $attributes['name'] : implode('-', $this->faker->words(3));
-//        $max_players = array_key_exists('max_players', $attributes) ? $attributes['max_players'] : $this->faker->numberBetween(1, 10);
-//        $game_id = "league-of-legends";
-//
-//        $I->fillField(['id' => 'name'], $name);
-//        $I->fillField(['id' => 'max_players'], $max_players);
-//        $I->selectOption(['id' => 'game_id'], $game_id);
-//        $I->click(['id' => 'submit']);
-//        return array($name, $max_players, $game_id);
-//    }
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function tryToCreateAPlayer(AcceptanceTester $I)
+    {
+        $I->wantTo('create a player on the management page');
+        list(
+            $name,
+            $username,
+            $email,
+            $phone,
+            $team,
+            $tournament,
+            $game
+            ) = $I->getAPlayerWithNoAssociations($I);
+
+        // check return, we should have a message and all the fields filled
+        $I->see('The player '.$name.' was added');
+
+    }
 
 }
