0439d75249b1aca1ed7ce84e8f055b4f4abd8dc1
Nelson <manklar@hotmail.com>
2016-10-13 12:07:09 -0500

    Bracket changes

--- resources/assets/js/bracketApp.js
+++ resources/assets/js/bracketApp.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 
         data: {
             playerTotal: 0,
+            added: '',
+            removed: '',
             playerOrderGeneral:0,
             playerNameForm: '',
             playerArray:[
@@ -20,13 +22,33 @@ $(document).ready(function() {
         },
         methods: {
             addPlayer: function(){
-                this.playerOrderGeneral+=1;
-                this.playerTotal +=1;
-                this.playerArray.push({playerName:this.playerNameForm, playerOrder:this.playerOrderGeneral});
-                this.playerNameForm= '';
+                if(this.playerNameForm !='') {
+                    this.playerOrderGeneral += 1;
+                    this.playerTotal += 1;
+                    this.playerArray.push({playerName: this.playerNameForm, playerOrder: this.playerOrderGeneral});
+                    this.playerNameForm = '';
+                    this.removed = '';
+                    this.added = this.playerNameForm;
+                }
             },
-            removePlayer: function(){
+            removePlayer: function(player){
+                // this.delete(this.playerArray, player);
+                var playerRemoved;
+                var array = [2, 5, 9];
+                var index = this.playerArray.indexOf(player);
+                if (index > -1) {
+                    playerRemoved = this.playerArray.splice(index, 1);
+                }
+alert(playerRemoved);
 
+                // for(var i=0; i<playerArray.length; i++){
+                //     if(playerArray[i] == player){
+                //         first = playerArray.splice(0,i);
+                //     }
+                // }
+                this.playerTotal -= 1;
+                this.removed = playerRemoved.playerName;
+                this.added = '';
             },
             createBracket: function(){
 
