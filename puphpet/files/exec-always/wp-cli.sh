a37a5831b9ae720bf18b0cde307abb42c451c2e6
Nelson <manklar@hotmail.com>
2016-10-13 12:30:40 -0500

    exec-always: Files are there now.

---
+++ puphpet/files/exec-always/wp-cli.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ~/
+seleniumScript="wp-cli.sh"
+seleniumScriptPath="${PWD}/${seleniumScript}"
+seleniumScriptDownload="https://raw.githubusercontent.com/paulbunyannet/bash/master/wp/${seleniumScript}"
+if [ -f "${seleniumScriptPath}" ]; then
+    sudo rm -f ${seleniumScriptPath}
+fi;
+sudo wget -O ${seleniumScriptPath} ${seleniumScriptDownload}
+. ${seleniumScriptPath}
