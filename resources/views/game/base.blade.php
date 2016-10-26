2f3a7ccb60292587a440469b4a7d653c9248cebe
Nelson <manklar@hotmail.com>
2016-10-26 13:54:52 -0500

    Base added input and email route

--- resources/views/game/base.blade.php
+++ resources/views/game/base.blade.php
@@ -23,6 +23,75 @@
         ul, li{
             list-style: none;
         }
+        /** ----------------------------------- **/
+        #search,
+        #submit {
+            float: left;
+        }
+
+        #search {
+            padding: 5px 9px;
+            height: 23px;
+            width: 180px;
+            border: 1px solid #fab04f;
+            color: #000000;
+            font: normal 13px 'trebuchet MS', arial, helvetica;
+            background: #f1f1f1;
+            border-radius: 50px 3px 3px 50px;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25) inset, 0 1px 0 rgba(255, 255, 255, 1);
+        }
+
+        /* ----------------------- */
+
+        #submit
+        {
+            background-color: #fab04f;
+            background-image: linear-gradient(#fab04f, #b06430);
+            border-radius: 3px 50px 50px 3px;
+            border-width: 1px;
+            border-style: solid;
+            border-color: #fefa86 #fed678 #fab04f;
+            box-shadow: 0 0 1px rgba(0, 0, 0, 0.3),
+            0 1px 0 rgba(255, 255, 255, 0.3) inset;
+            height: 23px;
+            margin: 0 0 0 -1px;
+            padding: 0;
+            width: 90px;
+            cursor: pointer;
+            font: bold 14px Arial, Helvetica;
+            color: #7b4a19;
+            text-shadow: 0 1px 0 rgba(255,255,255,0.5);
+        }
+
+        #submit:hover {
+            background-color: #fefa86;
+            background-image: linear-gradient(#b06430, #fab04f);
+        }
+
+        #submit:active {
+            background: #fefa86;
+            outline: none;
+            box-shadow: 0 1px 4px rgba(0, 0, 0, 0.5) inset;
+        }
+
+        #submit::-moz-focus-inner {
+            border: 0;  /* Small centering fix for Firefox */
+        }
+        #search::-webkit-input-placeholder {
+            color: #9c9c9c;
+            font-style: italic;
+        }
+
+        #search:-moz-placeholder {
+            color: #9c9c9c;
+            font-style: italic;
+        }
+
+        #search:-ms-placeholder {
+            color: #9c9c9c;
+            font-style: italic;
+        }
+
     </style>
     <script src="https://use.typekit.net/bhh0sxx.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
@@ -73,6 +142,12 @@
                         <li>{{ Html::linkAction('Backend\Manage\EmailController@email',
                         'Email Lists') }}
                         </li>
+                        <li>
+                            <form id="searchbox" action="">
+                                <input id="search" type="text" placeholder="Type here">
+                                <input id="submit" type="submit" value="Search">
+                            </form>
+                        </li>
 
                     </ul>
                 </div><!-- /.navbar-collapse -->
@@ -100,6 +175,9 @@
 //        });
 
         $(document).ready(function () {
+            $('#searchBar').change(function () {
+                $(this).val();
+            });
             $('#theIframe', window.parent.document).height($('#page-content').height()).css('min-height', 700);
 //            $('#wpfooter', window.parent.document).addClass('hidden');
             var toAdd = '</div><div id="pageLinker2" class="btn-group btn-group-justified" role="group" aria-label="Justified button group">';
