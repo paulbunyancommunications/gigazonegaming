5e09a5c5e37d037c7222a2903ac466d07316c0e6
Nelson <manklar@hotmail.com>
2016-10-26 13:54:29 -0500

    Bracket small changes

--- resources/assets/js/bracketApp.js
+++ resources/assets/js/bracketApp.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
         el: '#brackets',
 
         data: {
+            addOrUpdate: 'add',
+            tournamentNameForm: 'Tournament Name',
+            playerUsernameForm: '',
+            playerFirstNameForm: '',
+            playerLastNameForm: '',
+            playerPhoneForm: '',
+            playerEmailForm: '',
+            playerScoreForm: '',
             playerTotal: 0,
             added: '',
             removed: '',
@@ -14,19 +22,34 @@ $(document).ready(function() {
             playerNameForm: '',
             playerArray:[
                 // {
-                //     playerName: '',
-                //     playerLevel: 0,
-                //     playerOrder: 0
+                // Username: this.playerUsernameForm,
+                // FirstName: this.playerFirstNameForm,
+                // LastName: this.playerLastNameForm,
+                // Phone: this.playerPhoneForm,
+                // Email: this.playerEmailForm,
+                // Score: this.playerScoreForm
                 // }
             ]
         },
         methods: {
             addPlayer: function(){
-                if(this.playerNameForm !='') {
+                if(this.playerFirstNameForm !='' && this.playerLastNameForm !='' && this.playerUsernameForm !='') {
                     this.playerOrderGeneral += 1;
                     this.playerTotal += 1;
-                    this.playerArray.push({playerName: this.playerNameForm, playerOrder: this.playerOrderGeneral});
-                    this.playerNameForm = '';
+                    this.playerArray.push({
+                        Username: this.playerUsernameForm,
+                        FirstName: this.playerFirstNameForm,
+                        LastName: this.playerLastNameForm,
+                        Phone: this.playerPhoneForm,
+                        Email: this.playerEmailForm,
+                        Score: this.playerScoreForm
+                    });
+                    this.playerUsernameForm = '';
+                    this.playerFirstNameForm = '';
+                    this.playerLastNameForm = '';
+                    this.playerPhoneForm = '';
+                    this.playerEmailForm = '';
+                    this.playerScoreForm = '';
                     this.removed = '';
                     this.added = this.playerNameForm;
                 }
@@ -34,24 +57,57 @@ $(document).ready(function() {
             removePlayer: function(player){
                 // this.delete(this.playerArray, player);
                 var playerRemoved;
-                var array = [2, 5, 9];
                 var index = this.playerArray.indexOf(player);
                 if (index > -1) {
                     playerRemoved = this.playerArray.splice(index, 1);
                 }
-alert(playerRemoved);
-
-                // for(var i=0; i<playerArray.length; i++){
-                //     if(playerArray[i] == player){
-                //         first = playerArray.splice(0,i);
-                //     }
-                // }
                 this.playerTotal -= 1;
                 this.removed = playerRemoved.playerName;
                 this.added = '';
             },
+            updatePlayer: function(player){
+                // this.delete(this.playerArray, player);
+                var playerUpdate;
+                var index = this.playerArray.indexOf(player);
+                if (index > -1) {
+                    this.playerArray[index]['score'] = this.playerScoreForm;
+                }
+                this.removed = '';
+                this.added = '';
+                this.addOrUpdate = 'update'
+                this.playerUsernameForm = '';
+                this.playerFirstNameForm = '';
+                this.playerLastNameForm = '';
+                this.playerPhoneForm = '';
+                this.playerEmailForm = '';
+                this.playerScoreForm = '';
+                this.removed = '';
+                this.added = '';
+            },
+            updatePlayer: function(player){
+                // this.delete(this.playerArray, player);
+                var playerUpdate;
+                var index = this.playerArray.indexOf(player);
+                if (index > -1) {
+                    this.playerScoreForm = this.playerArray[index]['score'];
+                }
+                alert(this.playerArray[index]);
+                this.removed = '';
+                this.added = '';
+                this.addOrUpdate = 'add';
+                this.playerUsernameForm = '';
+                this.playerFirstNameForm = '';
+                this.playerLastNameForm = '';
+                this.playerPhoneForm = '';
+                this.playerEmailForm = '';
+                this.playerScoreForm = '';
+                this.removed = '';
+                this.added = '';
+            },
             createBracket: function(){
+                for(var i=0; this.playerArray.length > i; i++){
 
+                }
             }
         }
 
--- resources/views/game/bracket.blade.php
+++ resources/views/game/bracket.blade.php
@@ -16,17 +16,29 @@
         <div id="brackets">
             <div id="addPlayersDiv">
                 <div>
-                    <input type="text" id="playerNameForm" v-model="playerNameForm" />
-                    <button  v-on:click="addPlayer" class="btn btn-primary" >Add Player</button>
+                    <input type="text" id="tournamentNameForm" v-model="tournamentNameForm" placeholder="Tournament Name" />
                     <button  v-on:click="createBracket" class="btn btn-primary" >Create bracket</button>
                 </div>
+                <div>
+                    <input type="text" id="playerUsernameForm" v-model="playerUsernameForm" placeholder="Username" />
+                    <input type="text" id="playerFirstNameForm" v-model="playerFirstNameForm"  placeholder="First Name" />
+                    <input type="text" id="playerLastNameForm" v-model="playerLastNameForm"  placeholder="Last Name"/>
+                    <input type="text" id="playerPhoneForm" v-model="playerPhoneForm"  placeholder="Phone"/>
+                    <input type="email" id="playerEmailForm" v-model="playerEmailForm"  placeholder="Email"/>
+                    <input type="number" id="playerScoreForm" v-model="playerScoreForm"  placeholder="Score"/>
+                    <button v-if="addOrUpdate=='add'" v-on:click="addPlayer" class="btn btn-primary" > Add Player</button>
+                    <button v-if="addOrUpdate=='update'" v-on:click="saveUpdatedPlayer" class="btn btn-primary" > Update Player</button>
+                </div>
             </div>
-            <div>Players Registered: @{{ playerTotal }}</div>
+            <div v-if="tournamentNameForm!=''">Players Registered in @{{ tournamentNameForm }}: @{{ playerTotal }}</div>
             <div v-if="removed!=''">Last Player Removed: @{{ removed }}</div>
             <div v-if="added!=''">Last Player Added: @{{ added }}</div>
             <hr />
             <ul class="list-group" v-if="playerArray!=[]" >
-                <li v-for="playerX in playerArray" class='list-group-item'>@{{ playerX.playerName }}<button  v-on:click="removePlayer(playerX)">&#10007</button></li>
+                <li v-for="playerX in playerArray" class='list-group-item'>@{{ playerX.Username }}: @{{ playerX.FirstName }} @{{ playerX.LastName }}<button  v-on:click="removePlayer(playerX)">&#10007</button> <input type="number" :value = "playerX.Score" v-on:change="updatePlayer(playerX)"/></li>
+            </ul>
+            <ul class="list-group" v-if="playerArray!=[]" >
+                <li v-for="playerX in playerArray" class='list-group-item'>@{{ playerX.Username }}: @{{ playerX.FirstName }} @{{ playerX.LastName }} score: @{{ playerX.Score }}</li>
             </ul>
         </div>
     </div>
