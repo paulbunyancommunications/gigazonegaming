1eb162b5444693cbff34daccb06c44ab3c5e3a69
Nate Nolting <me@natenolting.com>
2016-10-28 11:59:49 -0500

    added check to see if old enc file does not match new enc file

--- encrypt-files.sh
+++ encrypt-files.sh
@@ -27,5 +27,12 @@ source ${ws}/enc-files
 for ((i=0; i<${#prefix[@]}; i++))
 do
     echo "Encrypting: ${ws}/${prefix[i]}${suffix[i]}, ${desc[i]}"
-    openssl enc -aes-256-cbc -salt -in ${ws}/${prefix[i]}${suffix[i]} -out ${ws}/${prefix[i]}.enc -pass pass:${password}
+    openssl enc -aes-256-cbc -salt -in ${ws}/${prefix[i]}${suffix[i]} -out ${ws}/${prefix[i]}.enc.temp -pass pass:${password}
+    if [ -f ${ws}/${prefix[i]}.enc ]
+    then
+        mv ${ws}/${prefix[i]}.enc.temp ${ws}/${prefix[i]}.enc
+        continue
+    fi
+    cmp --silent ${ws}/${prefix[i]}.enc.temp ${ws}/${prefix[i]}.enc || echo "${ws}/${prefix[i]}.enc does not match new encrypted file."; mv ${ws}/${prefix[i]}.enc.temp ${ws}/${prefix[i]}.enc
+
 done
