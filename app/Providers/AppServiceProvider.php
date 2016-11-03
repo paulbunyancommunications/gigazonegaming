5b88299937487f4e0b962651c8f294229a857c55
Nate Nolting <me@natenolting.com>
2016-11-03 12:58:21 -0500

    added blade directive for auto versioning asset files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Blade::directive('autoVersion', function($expression) {
+            return "<?php echo \\Pbc\\AutoVersionSingle::file($expression); ?>";
+        });
     }
 
     /**
