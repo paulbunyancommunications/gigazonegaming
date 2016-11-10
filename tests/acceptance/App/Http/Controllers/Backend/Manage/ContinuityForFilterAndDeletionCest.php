4a62f6b6e19a46f89de773d2ca713cdd2c7b3f06
Nelson <manklar@hotmail.com>
2016-11-10 11:51:32 -0600

    Push

--- tests/acceptance/App/Http/Controllers/Backend/Manage/ContinuityForFilterAndDeletionCest.php
+++ tests/acceptance/App/Http/Controllers/Backend/Manage/ContinuityForFilterAndDeletionCest.php
@@ -50,7 +50,7 @@ class ContinuityForFilterAndDeletionCest extends BaseAcceptance
     {
         $I->wantTo('click on a game and land on tournament page with an applied filter');
         $I->amOnPage('/app/manage/game');
-        $I->waitForJS('if(!window.jQuery){'.
+        $I->waitForJS('return if(!window.jQuery){'.
             'var script = document.createElement("script");'.
             'script.type = "text/javascript";'.
             'script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";'.
