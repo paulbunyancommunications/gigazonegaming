146125d08fa2e118b7039e70409d16471892cb84
Nate Nolting <me@natenolting.com>
2016-11-30 15:56:38 -0600

    trying again to get library copier working

--- .npm_copy_libraries.sh
+++ .npm_copy_libraries.sh
@@ -13,14 +13,14 @@ mkdir ${publicRoot}/app/content/libraries || true
 mkdir ${resourceRoot}/assets/sass/libraries || true
 
 from=(
-    ${nodeRoot}/bootstrap/dist/
-    ${nodeRoot}/bootstrap/dist/
-    ${nodeRoot}/bootstrap-sass/assets/
-    ${nodeRoot}/bootstrap-sass/assets/
-    ${bowerRoot}/bourbon/app/assets/stylesheets/
-    ${bowerRoot}/bourbon/app/assets/stylesheets/
-    ${bowerRoot}/Gutenberg/src/style/
-    ${bowerRoot}/Gutenberg/src/style/
+    ${nodeRoot}/bootstrap/dist
+    ${nodeRoot}/bootstrap/dist
+    ${nodeRoot}/bootstrap-sass/assets
+    ${nodeRoot}/bootstrap-sass/assets
+    ${bowerRoot}/bourbon/app/assets/stylesheets
+    ${bowerRoot}/bourbon/app/assets/stylesheets
+    ${bowerRoot}/Gutenberg/src/style
+    ${bowerRoot}/Gutenberg/src/style
 )
 to=(
     ${publicRoot}/${themeFolder}/libraries/bootstrap
@@ -35,5 +35,5 @@ to=(
 
 for ((i=0; i<${#from[@]}; i++))
 do
-    mkdir ${to[i]} || true && cp -r ${from[i]} ${to[i]}
+    mkdir ${to[i]} || true && cp -R "${from[i]}"/. "${to[i]}"/
 done
