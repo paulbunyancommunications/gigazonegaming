290ba20620d4d680325e4ec7fcd39f2204c6d463
Nate Nolting <me@natenolting.com>
2016-12-01 14:28:54 -0600

    added phan for code analysis https://github.com/etsy/phan

---
+++ .phan/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'directory_list' => [
+        'vendor',
+		'src',
+		'app',
+		'public_html/wp-content/themes/gigazone-gaming',
+	],
+	'exclude_analysis_directory_list' => [
+	    'vendor'
+    ],
+];
