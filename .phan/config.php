47cffe0870362a30d4a3921ecd2e9c58a115da46
Nate Nolting <me@natenolting.com>
2016-12-02 11:01:27 -0600

    Updated PHAN config to find functions in Wordpress but not to check them

--- .phan/config.php
+++ .phan/config.php
@@ -2,11 +2,13 @@
 return [
 	'directory_list' => [
         'vendor',
+        'public_html/wp',
 		'src',
 		'app',
 		'public_html/wp-content/themes/gigazone-gaming',
 	],
 	'exclude_analysis_directory_list' => [
-	    'vendor'
+	    'vendor',
+        'public_html/wp'
     ],
 ];
