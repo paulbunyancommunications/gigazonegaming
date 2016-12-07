5fc0af515f32557bf7067999e1b17802b638ad35
Nate Nolting <me@natenolting.com>
2016-12-07 12:23:53 -0600

    forgot to give file path to php.ini when enabling the ast extension in the vagrant box

--- puphpet/files/exec-always/install-php-ast-extension.sh
+++ puphpet/files/exec-always/install-php-ast-extension.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+File="/etc/php.ini"
 if ! grep -q extension=ast.so "$File"; then
     # Install php-ast extension
     sudo git clone https://github.com/nikic/php-ast.git
@@ -7,5 +8,5 @@ if ! grep -q extension=ast.so "$File"; then
     sudo ./configure
     sudo make install
     #http://stackoverflow.com/a/550808/405758
-    echo "extension=ast.so" | sudo tee --append /etc/php.ini
+    echo "extension=ast.so" | sudo tee --append ${File}
 fi;
