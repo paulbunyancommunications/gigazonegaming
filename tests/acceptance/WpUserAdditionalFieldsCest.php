888e07955f29c94adc68b039cbe64b1dde4b0f43
Nate Nolting <me@natenolting.com>
2016-12-12 09:23:47 -0600

    reverted back to old test code and added scroll to actions

--- tests/acceptance/WpUserAdditionalFieldsCest.php
+++ tests/acceptance/WpUserAdditionalFieldsCest.php
@@ -73,18 +73,18 @@ class WpUserAdditionalFieldsCest extends \BaseAcceptance
             $I->fillField(['id' => $fields[$i]], $fieldValue);
             $I->scrollTo(['id' => $fields[$i].'_profile']);
             $I->fillField(['id' => $fields[$i].'_profile'], $fieldProfileValue);
-            $I->scrollTo(['id' => 'submit']);
             $I->click(['id' => 'submit']);
             // make a new post and check to see that the field are missing on page with the profile shortcode
             $I->amOnPage('/wp/wp-admin/post-new.php');
             $I->fillField(['id' => 'title'], $faker->sentence);
+            $I->click(['id' => 'content-html']);
+            $I->wait(1);
             $I->fillField(['id' => 'content'], '[user-profile id="'.$this->wpAdminUser['name'].'"]');
-            $I->waitForElementVisible(['id' => 'publish'], self::TEXT_WAIT_TIMEOUT*3);
+            $I->wait(3);
             $I->click(['id' => 'publish']);
-            $I->waitForText('Post published', self::TEXT_WAIT_TIMEOUT*3);
-            // now go to the posted page and make sure this social element is not on the page
+            $I->see('Post published');
             $I->click('#sample-permalink a');
-            $I->waitForElementVisible(['class' => 'masthead-logo'], self::TEXT_WAIT_TIMEOUT*3);
+            $I->wait(1);
             $I->dontSeeElementInDOM(['class' => 'user-profile--'.$this->wpAdminUser['name'].'-'.$fields[$i]]);
             $I->dontSee(ucfirst($fields[$i]));
             $I->amOnPage('/wp/wp-admin/profile.php');
@@ -106,7 +106,9 @@ class WpUserAdditionalFieldsCest extends \BaseAcceptance
         for($i=0; $i < count($fields); $i++) {
             $fieldValue = $faker->userName;
             $fieldProfileValue = $faker->url;
+            $I->scrollTo(['id' => $fields[$i]]);
             $I->fillField(['id' => $fields[$i]], $fieldValue);
+            $I->scrollTo(['id' => $fields[$i].'_profile']);
             $I->fillField(['id' => $fields[$i].'_profile'], $fieldProfileValue);
             $I->click(['id' => 'submit']);
 
