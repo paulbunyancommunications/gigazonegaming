b4e990888ba76b9f27b13cd2a2f56257afc76a60
romanm <romanm@paulbunyan.net>
2017-05-23 18:02:56 -0500

    modified css

--- resources/views/LeagueOfLegends/DisplayTeam.blade.php
+++ resources/views/LeagueOfLegends/DisplayTeam.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <?php
-        $count = 1;
+        $count = 0;
         $playerNames = array(0=> 'KingMorpheus2131','Spartan7Warrior','TheDestroyerOfWorlds','CoolCat56','Dominator6789');
         $icons = array(0=> 'http://news.cdn.leagueoflegends.com/public/images/articles/2014/november_2014/sru-beta/Red.jpg','http://news.cdn.leagueoflegends.com/public/images/articles/2014/november_2014/sru-beta/Red.jpg','http://news.cdn.leagueoflegends.com/public/images/articles/2014/november_2014/sru-beta/Red.jpg','http://news.cdn.leagueoflegends.com/public/images/articles/2014/november_2014/sru-beta/Red.jpg','http://news.cdn.leagueoflegends.com/public/images/articles/2014/november_2014/sru-beta/Red.jpg');
         $rank = array(0=>'1','2','3','4','5');
@@ -13,15 +13,11 @@
             $images = array(0=>'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Diana_0.jpg','http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Rengar_0.jpg','http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Ezreal_0.jpg','http://ddragon.leagueoflegends.com/cdn/img/champion/loading/LeeSin_0.jpg','http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Alistar_0.jpg');
         else
             $images = array(0=>'/LeagueOfLegendsDisplay/Images/GZG-Atom-to-Animate.gif','/LeagueOfLegendsDisplay/Images/GZG-Atom-to-Animate.gif','/LeagueOfLegendsDisplay/Images/GZG-Atom-to-Animate.gif','/LeagueOfLegendsDisplay/Images/GZG-Atom-to-Animate.gif','/LeagueOfLegendsDisplay/Images/GZG-Atom-to-Animate.gif');
-        for ($i=0;$i<count($playerNames);$i++){
-            echo "<div class='col-lg' style='display:inline-block;float:none;'>";
-            echo '<div><div><b class="summonerName">'.$playerNames[$i].'</b><img class="icon" src="'. $icons[$i] .'"/></div></div>';
-            echo '<div><div class="playerInfo"><b class="WinLoss">Solo:</b></div></div>';
-            echo '<div><div class="playerInfo"><b class="WinLoss">Win/Loss: '.$winLoss[$i].'</b> <b class="rank">Rank: '.$rank[$i].'</b></div></div>';
-            echo '<div><div class="playerInfo"><b class="WinLoss">Flex:</b></div></div>';
-            echo '<div><div class="playerInfo"><b class="WinLoss">Win/Loss: '.$flexWinLoss[$i].'</b> <b class="rank">Rank: '.$flexRank[$i].'</b></div></div>';
-            echo '<div><div class="spinner-container"><img class="image" src="'.$images[$i].'"/></div></div>';
-            echo "</div>";
-        }
     ?>
+@stop
+@section('image')
+    <?php echo '/LeagueOfLegendsDisplay/Images/GZG-Atom-to-Animate.gif'; ?>
+@stop
+@section('icon')
+    <?php echo 'http://news.cdn.leagueoflegends.com/public/images/articles/2014/november_2014/sru-beta/Red.jpg'; ?>
 @stop
