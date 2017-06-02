1c33b5b90cc4003ebc0c918d752f38988c0742b8
romanm <romanm@paulbunyan.net>
2017-06-02 16:00:55 -0500

    created .scss files for transfer from simple css, added jquery function that displays looping image for team name background, deleted unnecessary images, made a few changes to layout blade, such as deleting and adding lines

--- public_html/LeagueOfLegendsDisplay/JS/teamDisplay.js
+++ public_html/LeagueOfLegendsDisplay/JS/teamDisplay.js
@@ -1,7 +1,9 @@
 /* Created by Roman on 5/22/17.
  * added comment
  */
+/*This is where the js is for the start page*/
 
+/*This allows the display of teams and colors after a valid tournament is selected*/
 function showTeams(){
     $("#Team").children('option').each(function(i) {
         $('#submit').addClass('hidden');
@@ -9,7 +11,6 @@ function showTeams(){
         var counter = 0;
         if($(this).attr('t_id') == id){
             $(this).removeClass('hidden');
-            //$('#submit').removeClass('hidden');
             $('#Color').removeClass('hidden');
             counter++;
         }else{
@@ -20,17 +21,14 @@ function showTeams(){
                 $("#defaultColor").attr('selected','selected');
             }else {
                 $(this).addClass('hidden');
-                //$('#submit').addClass('hidden');
                 $('#Color').addClass('hidden');
             }
         }
         if(counter == 0){
             $("#Team").addClass("hidden");
-            //$('#submit').addClass('hidden');
             $('#Color').addClass('hidden');
         }else{
             $("#Team").removeClass("hidden");
-            // $('#submit').removeClass('hidden');
             $('#Color').removeClass('hidden');
         }
     });
@@ -38,6 +36,7 @@ function showTeams(){
 }
 showTeams();
 
+/* These three blocks make sure there is a valid selection in all of the dropdowns before the submit button is displayed */
 $('#Color').change(function() {
     if($("#Color option:selected").text() !== 'Select a Color' && $("#Team option:selected").text() !== 'Select a Team') {
         $('#submit').removeClass('hidden');
@@ -66,6 +65,8 @@ $('#Tournament').change(function() {
 function teamView(){
     window.open('https://gigazonegaming.localhost/app/GameDisplay/'+$( '#Tournament option:selected').text()+'/'+$( '#Team option:selected').text()+'/'+$( '#Color option:selected').text());
 }
+
+/*This begins the js that is used on the team display page*/
 setTimeout(
     function(){
         $('#divA0').fadeOut(2000);
@@ -74,7 +75,7 @@ setTimeout(
         $('#divA3').fadeOut(2000);
         $('#divA4').fadeOut(2000);
     },
-    2000
+    5000
 );
 setTimeout(
     function() {
@@ -84,5 +85,21 @@ setTimeout(
         $('#divB3').fadeIn(3000);
         $('#divB4').fadeIn(3000);
     },
-    4100
+    7100
 );
+
+function showBackground(){
+
+    $('.backgroundH1').animate({
+        'background-position-x': '0%',
+        'background-position-y': '100%'
+    }, 50000, 'linear',showBackground2);
+};
+function showBackground2(){
+
+    $('.backgroundH1').animate({
+        'background-position-x': '0%',
+        'background-position-y': '0%'
+    }, 50000, 'linear',showBackground);
+};
+showBackground();
