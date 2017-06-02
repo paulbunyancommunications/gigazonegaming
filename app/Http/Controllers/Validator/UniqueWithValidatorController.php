6c7270fb22f1e6420b358bc217e05e42de555f88
Nelson <manklar@hotmail.com>
2017-06-02 12:25:16 -0500

    remove new package as this did not help at all

---
+++ app/Http/Controllers/Validator/UniqueWithValidatorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Validator;
+
+use App\Http\Controllers\Controller;
+
+class UniqueWithValidatorController extends Controller
+{
+    /*
+     *
+     * @param $attribute "selected attribute ie: 'name" => ...,
+     * @param $value "value passed from request for this exact element",
+     * @param $parameters "parameters passed along:
+     *              1_ connection.table or table",
+     *              2_ column to look at in db for the passed attribute
+     *              3_ 4_ 5_ and so on will be like such:
+     *                       '= tableColumnName' if the name of the key with the "unique with" constrain name is different to the table column name,
+     *                       'key_passed = table_column' for all following request keys that also differ from the table column name
+     *                       'key_passed' pass for all the request keys matching the table column
+     *
+     *                  !all spaces will be trimmed before and after so the "=" can or not have space before or after
+     * @param $validator "we wont use this as all things inside it are protected",
+     * @notAParam $_REQUEST 'request holds all the passed values so we will use this to match all keys to the respective values and prevent injections :) '
+     * @return boolean
+     */
+    public function validateComposeKey($attribute, $value, $parameters, $validator)
+    {
+        $counter = count($parameters); // this should be quick, there should be less than 3 parameters and no more than 3 constraints in the unique compose key, either way less than 20 will still be pretty fast counting
+
+//        dd(
+//            "selected attribute",
+//            $attribute,
+//            "value passed",
+//            $value,
+//            "parameters passed along",
+//            $parameters,
+//            "validator",
+//            $validator,
+//            'request',
+//            $_REQUEST,
+//            'counter',
+//            $counter
+//        );
+        if ($counter > 1) {
+            $connectionDbTable = trim($parameters[0]);
+            $dbColumn = trim($parameters[1]);
+            $query = "";
+            //create table where for each so we see through the elements if exist return false is not return true
+            $theConnectionArray = explode('.',$connectionDbTable);
+//            dd($connectionDbTable, $dbColumn, $theConnectionArray);
+            if ( count( $theConnectionArray ) == 1 ) {
+                $query = \DB::table($connectionDbTable[0]);
+            }elseif ( count( $theConnectionArray ) == 2 ) {
+                $query = \DB::connection($theConnectionArray[0])->table($theConnectionArray[1]);
+            }
+//            dd($query->toSql());
+            $forQuery = [];
+            for ($i=1; $i < $counter; $i++) {
+                //todo parse through this
+                $equal = strpos($parameters[$i], '=');
+                $col="";$val="";
+                if( $equal === false ){ // request key is same that column name
+                    $col = trim($parameters[$i]);
+                    $val = trim($_REQUEST[$col]);
+                }else{
+                    $expression = explode('=', $parameters[$i]);
+                    if($equal == 0 ){ // =column name
+                        $col = trim($expression[0]);
+                        $val = trim($_REQUEST[$attribute]);
+                    }else{
+                        $col = trim($expression[1]);
+                        $val = trim($expression[0]);
+                    }
+                }
+                if( $col != "" and $val != "" ) {
+                    array_push($forQuery, [$col, '=', $val]);
+                }
+            }
+            $exists = !($query->where($forQuery)->exists());
+
+            return $exists;
+        }
+        return false;
+    }
+}
