8775671fa1114f48cef8d7ff759d54972b1c7086
romanm <romanm@paulbunyan.net>
2017-06-06 10:12:56 -0500

    made changes so that player info is collapseable

--- public_html/LeagueOfLegendsDisplay/JS/teamDisplay.js
+++ public_html/LeagueOfLegendsDisplay/JS/teamDisplay.js
@@ -69,23 +69,23 @@ function teamView(){
 /*This begins the js that is used on the team display page*/
 setTimeout(
     function(){
-        $('#divA0').fadeOut(2000);
-        $('#divA1').fadeOut(2000);
-        $('#divA2').fadeOut(2000);
-        $('#divA3').fadeOut(2000);
-        $('#divA4').fadeOut(2000);
+        $('#divA0').fadeOut(1000);
+        $('#divA1').fadeOut(1000);
+        $('#divA2').fadeOut(1000);
+        $('#divA3').fadeOut(1000);
+        $('#divA4').fadeOut(1000);
     },
-    5000
+    1000
 );
 setTimeout(
     function() {
-        $('#divB0').fadeIn(3000);
-        $('#divB1').fadeIn(3000);
-        $('#divB2').fadeIn(3000);
-        $('#divB3').fadeIn(3000);
-        $('#divB4').fadeIn(3000);
+        $('#divB0').fadeIn(1000);
+        $('#divB1').fadeIn(1000);
+        $('#divB2').fadeIn(1000);
+        $('#divB3').fadeIn(1000);
+        $('#divB4').fadeIn(1000);
     },
-    7100
+    2100
 );
 
 function showBackground(){
@@ -103,3 +103,48 @@ function showBackground2(){
     }, 50000, 'linear',showBackground);
 };
 showBackground();
+
+/*This is used to collapse the columns so that they are smaller allowing for more info on the page if wanted*/
+$('.championImage').click( function(){
+    $('#'+this.id+'-0').addClass('hidden');
+    $('#'+this.id+'-1').addClass('hidden');
+    $('#'+this.id+'-2').addClass('hidden');
+    $('#'+this.id+'-3').removeClass('hidden');
+    $('#'+this.id+'-4').addClass('v-align');
+});
+$('#0-3').click( function(){
+    $('#0-0').removeClass('hidden');
+    $('#0-1').removeClass('hidden');
+    $('#0-2').removeClass('hidden');
+    $('#0-4').removeClass('v-align');
+    $('#'+this.id).addClass('hidden');
+});
+$('#1-3').click( function(){
+    $('#1-0').removeClass('hidden');
+    $('#1-1').removeClass('hidden');
+    $('#1-2').removeClass('hidden');
+    $('#1-4').removeClass('v-align');
+    $('#'+this.id).addClass('hidden');
+});
+$('#2-3').click( function(){
+    $('#2-0').removeClass('hidden');
+    $('#2-1').removeClass('hidden');
+    $('#2-2').removeClass('hidden');
+    $('#2-4').removeClass('v-align');
+    $('#'+this.id).addClass('hidden');
+});
+$('#3-3').click( function(){
+    $('#3-0').removeClass('hidden');
+    $('#3-1').removeClass('hidden');
+    $('#3-2').removeClass('hidden');
+    $('#3-4').removeClass('v-align');
+    $('#'+this.id).addClass('hidden');
+});
+$('#4-3').click( function(){
+    $('#4-0').removeClass('hidden');
+    $('#4-1').removeClass('hidden');
+    $('#4-2').removeClass('hidden');
+    $('#4-4').removeClass('v-align');
+    $('#'+this.id).addClass('hidden');
+});
+
--- resources/views/LeagueOfLegends/DisplayTeam.blade.php
+++ resources/views/LeagueOfLegends/DisplayTeam.blade.php
@@ -17,21 +17,21 @@
     @stop
     @section('Player' . $i . 'SoloRank')
         @if(strpos($soloRankArray[$i],"BRONZE" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"SILVER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/1_3.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/1_3.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"GOLD" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/goldv.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/goldv.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"PLATINUM" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/platinumv.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/platinumv.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"DIAMOND" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/diamondi.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/diamondi.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"MASTER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/master.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/master.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"CHALLENGER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/challenger.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/challenger.png"/><br/>{{$soloRankArray[$i]}}
         @else
-            <img class="rank unranked" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><span class="rankV">{{$soloRankArray[$i]}}</span>
+            <img class="rank unranked" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$soloRankArray[$i]}}
         @endif
     @stop
     @section('Player' . $i . 'SoloWinLoss')
@@ -39,21 +39,21 @@
     @stop
     @section('Player' . $i . 'FlexRank')
         @if(strpos($flexRankArray[$i],"BRONZE" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"SILVER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/1_3.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/1_3.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"GOLD" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/goldv.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/goldv.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"PLATINUM" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/platinumv.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/platinumv.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"DIAMOND" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/diamondi.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/diamondi.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"MASTER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/master.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/master.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"CHALLENGER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/challenger.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/challenger.png"/><br/>{{$flexRankArray[$i]}}
         @else
-            <img class="rank unranked" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><span class="rankV">{{$flexRankArray[$i]}}</span>
+            <img class="rank unranked" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$flexRankArray[$i]}}
         @endif
     @stop
     @section('Player' . $i . 'FlexWinLoss')
