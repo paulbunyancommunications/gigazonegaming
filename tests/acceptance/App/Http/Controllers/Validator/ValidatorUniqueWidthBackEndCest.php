b23e03d69707642a98e4ef3933a65ca14d5b04cb
Nelson <manklar@hotmail.com>
2017-06-08 02:55:47 -0500

    multiple changes, testing passing

--- tests/acceptance/App/Http/Controllers/Validator/ValidatorUniqueWidthBackEndCest.php
+++ tests/acceptance/App/Http/Controllers/Validator/ValidatorUniqueWidthBackEndCest.php
@@ -12,13 +12,11 @@ class ValidatorUniqueWidthBackEndCest extends BaseAcceptance
      *
      */
     public $faker;
-    const DEFAULT_WAIT = 15;
+    const DEFAULT_WAIT = 30;
     const TEAM_A_NAME = "Tester Team Unique Width A";//same as in the SignUpUniqueWithValidatorTesterSeeder
     const TEAM_B_NAME = "Tester Team Unique Width B";//same as in the SignUpUniqueWithValidatorTesterSeeder
     const TOURNAMENT_A_NAME = "Tester Tournament Unique Width A";//same as in the SignUpUniqueWithValidatorTesterSeeder
     const TOURNAMENT_B_NAME = "Tester Tournament Unique Width B";//same as in the SignUpUniqueWithValidatorTesterSeeder
-    /**
-    public $faker;
 
     /**
      * Create the test admin user
@@ -26,7 +24,6 @@ class ValidatorUniqueWidthBackEndCest extends BaseAcceptance
     protected function populateDB(AcceptanceTester $I)
     {
 //        $I->runShellCommand('cd /var/www');
-        exec('php artisan db:seed --class=DatabaseSeeder');
         exec('php artisan db:seed --class=SignUpUniqueWithValidatorTesterSeeder');
         $this->faker = \Faker\Factory::create();
     }
@@ -38,12 +35,7 @@ class ValidatorUniqueWidthBackEndCest extends BaseAcceptance
         parent::_before($I);
         $this->populateDB($I);
         $this->loginWithAdminUser($I);
-        $I->am("admin");
-        $I->wantTo("A Admin User and I will create a team");
         $I->amOnPage('/app/manage/team');
-        $I->fillField("#name", $this::TEAM_A_NAME);
-        $I->selectOption("#tournament_id", "1"); //gz2016
-        $I->click(['id'=>"submit"]);
     }
 
     /**
@@ -59,8 +51,26 @@ class ValidatorUniqueWidthBackEndCest extends BaseAcceptance
      * Test the form with the participation flag
      * @param AcceptanceTester $I
      */
-    public function tryToCheckIfAfterCreatingATeamICanAssignItTheSameName(AcceptanceTester $I)
+    public function tryToCheckIfAfterCreatingATeamIfICanAssignAnotherTeamTheSameNameButHavingADifferentTournament(AcceptanceTester $I)
     {
-
+        $I->am("admin");
+        $I->wantTo("create a team");
+        $I->fillField("#name", $this::TEAM_A_NAME);
+        $I->selectOption("#tournament_id", array('text' => $this::TOURNAMENT_B_NAME)); //gz2016
+        $I->click(['id'=>"submit"]);
+        $I->waitForText("The team ".$this::TEAM_A_NAME." was added", $this::DEFAULT_WAIT);
+    }
+    /**
+     * Test the form with the participation flag
+     * @param AcceptanceTester $I
+     */
+    public function tryToCheckIfAfterCreatingATeamIfICanAssignAnotherTeamTheSameNameInTheSameTournament(AcceptanceTester $I)
+    {
+        $I->am("admin");
+        $I->wantTo("create a team");
+        $I->fillField("#name", $this::TEAM_A_NAME);
+        $I->selectOption("#tournament_id", array('text' => $this::TOURNAMENT_A_NAME)); //gz2016
+        $I->click(['id'=>"submit"]);
+        $I->waitForText("A team with the exact same name already exists for this tournament, please select a different name.", $this::DEFAULT_WAIT);
     }
 }
