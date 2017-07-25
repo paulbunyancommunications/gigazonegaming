203430fc24e97eef4ef68429f9d317f1205115d9
romanm <romanm@paulbunyan.net>
2017-07-25 16:50:43 -0500

    added terminating semicolons to gulp file Added report all errors to functions

--- public_html/wp-content/themes/gigazone-gaming/functions.php
+++ public_html/wp-content/themes/gigazone-gaming/functions.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 global $cachePool;
 // add wordpress settings page action for this theme
 require_once __DIR__ . '/theme-panel.php';
