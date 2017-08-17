fdae04bcd93c11e07b57feb9626ca0c4a2e5f4da
Nelson <manklar@hotmail.com>
2017-08-16 21:17:39 -0500

    fixed some errors in test

--- tests/acceptance/App/Http/Controllers/Backend/Manage/SearchController/SearchControllerForPlayerCest.php
+++ tests/acceptance/App/Http/Controllers/Backend/Manage/SearchController/SearchControllerForPlayerCest.php
@@ -1,16 +1,17 @@
 <?php
-namespace Tests\Acceptance\App\Http\Controllers\Backend\Manage;
+namespace Tests\Acceptance\App\Http\Controllers\Backend\Manage\SearchController;
 
 use \AcceptanceTester;
 use \BaseAcceptance;
+use \Faker\Factory as Faker;
 
 /**
  * Class SearchControllerForPlayerCest
  * @package Tests\Acceptance\App\Http\Controllers\Backend\Manage
  */
-class SearchControllerForPlayerCest extends BaseAcceptance
+class SearchControllerForPlayerCest extends \BaseAcceptance
 {
-
+    public $faker;
     /**
      * @param AcceptanceTester $I
      */
@@ -19,19 +20,18 @@ class SearchControllerForPlayerCest extends BaseAcceptance
         parent::_before($I);
         $this->populateDB($I);
         $this->loginWithAdminUser($I);
+        $this->faker = Faker::create();
         $I->amOnPage('/app/manage/player');
-
     }
 
     /**
      * Create the test admin user
+     * @param AcceptanceTester $I
      */
     protected function populateDB(AcceptanceTester $I)
     {
-//        $I->runShellCommand('cd /var/www');
         exec('php artisan db:seed --class=DatabaseSeeder');
         exec('php artisan db:seed --class=SearchControllerSeeder');
-        $this->faker = \Faker\Factory::create();
     }
 
     /**
@@ -48,27 +48,33 @@ class SearchControllerForPlayerCest extends BaseAcceptance
      * @test
      * @group SearchController
      * @group SearchControllerForPlayer
+     * @param AcceptanceTester $I
      */
     public function testSearchControllerGetAPlayerByFullNameSearch(AcceptanceTester $I)
     {
-        $faker = \Faker\Factory::create();
         $I->wantTo('Check that we can get a player from the search bar by full name');
         $I->amOnPage('/app/manage/player');
-        $firstName = $faker->firstName;
-        $lastName = $faker->lastName;
-        $username = $faker->userName;
-        $email = $faker->email;
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $username = $this->faker->userName;
+        $email = $this->faker->email;
         $I->fillField(['id' => 'name'], $firstName . ' ' . $lastName);
         $I->fillField(['id' => 'username'], $username);
         $I->fillField(['id' => 'email'], $email);
         $I->click(['id' => 'submit']);
 
+//        dd("here");
+        $I->waitForElementVisible('a[title="Edit player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+//        $id = $I->waitForJS("return document.querySelector('[title=\"Edit player " . $firstName . " " . $lastName ."\"]').id;)", $this::TEXT_WAIT_TIMEOUT);
         /** DO by full first name and last name */
         $I->fillField(['id' => 'searchText'], $firstName . ' ' . $lastName);
         $I->click(['id' => 'doSearchBoxSubmit']);
         $I->waitForElementVisible(['id' => 'searchResults'], BaseAcceptance::TEXT_WAIT_TIMEOUT);
-        $I->wait(1);
-        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForElementVisible('a[title="Player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->click('a[title="Player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForText("Update Player:", $this::TEXT_WAIT_TIMEOUT);
+        $I->see("Update Player: “". $firstName . ' ' . $lastName ."”");
 
     }
 
@@ -77,28 +83,32 @@ class SearchControllerForPlayerCest extends BaseAcceptance
      * @test
      * @group SearchController
      * @group SearchControllerForPlayer
+     * @param AcceptanceTester $I
      */
     public function testSearchControllerGetAPlayerByEmailSearch(AcceptanceTester $I)
     {
-        $faker = \Faker\Factory::create();
         $I->wantTo('Check that we can get a player from the search bar by email');
         $I->amOnPage('/app/manage/player');
-        $firstName = $faker->firstName;
-        $lastName = $faker->lastName;
-        $username = $faker->userName;
-        $email = $faker->email;
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $username = $this->faker->userName;
+        $email = $this->faker->email;
         $I->fillField(['id' => 'name'], $firstName . ' ' . $lastName);
         $I->fillField(['id' => 'username'], $username);
         $I->fillField(['id' => 'email'], $email);
         $I->click(['id' => 'submit']);
 
+        $I->waitForElementVisible('a[title="Edit player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+//        $id = $I->waitForJS("return document.querySelector('[title=\"Edit player " . $firstName . " " . $lastName ."\"]').id;)", $this::TEXT_WAIT_TIMEOUT);
         /** Do search by email */
         $I->fillField(['id' => 'searchText'], $email);
         $I->click(['id' => 'doSearchBoxSubmit']);
         $I->waitForElementVisible(['id' => 'searchResults'], BaseAcceptance::TEXT_WAIT_TIMEOUT);
-        $I->wait(1);
-        $I->waitForElementVisible('a[title="Edit player '. $firstName . ' ' . $lastName .'"]',BaseAcceptance::TEXT_WAIT_TIMEOUT);
-        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForElementVisible('a[title="Player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->click('a[title="Player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForText("Update Player:", $this::TEXT_WAIT_TIMEOUT);
+        $I->see("Update Player: “". $firstName . ' ' . $lastName ."”");
 
     }
 
@@ -107,28 +117,32 @@ class SearchControllerForPlayerCest extends BaseAcceptance
      * @test
      * @group SearchController
      * @group SearchControllerForPlayer
+     * @param AcceptanceTester $I
      */
     public function testSearchControllerGetAPlayerByUserNameSearch(AcceptanceTester $I)
     {
-        $faker = \Faker\Factory::create();
         $I->wantTo('Check that we can get a player from the search bar by username');
         $I->amOnPage('/app/manage/player');
-        $firstName = $faker->firstName;
-        $lastName = $faker->lastName;
-        $username = $faker->userName;
-        $email = $faker->email;
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $username = $this->faker->userName;
+        $email = $this->faker->email;
         $I->fillField(['id' => 'name'], $firstName . ' ' . $lastName);
         $I->fillField(['id' => 'username'], $username);
         $I->fillField(['id' => 'email'], $email);
         $I->click(['id' => 'submit']);
 
+        $I->waitForElementVisible('a[title="Edit player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+//        $id = $I->waitForJS("return document.querySelector('[title=\"Edit player " . $firstName . " " . $lastName ."\"]').id;)", $this::TEXT_WAIT_TIMEOUT);
         /** Do search by username */
         $I->fillField(['id' => 'searchText'], $username);
         $I->click(['id' => 'doSearchBoxSubmit']);
         $I->waitForElementVisible(['id' => 'searchResults'], BaseAcceptance::TEXT_WAIT_TIMEOUT);
-        $I->wait(1);
-        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
-
+        $I->waitForElementVisible('a[title="Player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->click('a[title="Player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForText("Update Player:", $this::TEXT_WAIT_TIMEOUT);
+        $I->see("Update Player: “". $firstName . ' ' . $lastName ."”");
     }
 
     /**
@@ -137,27 +151,33 @@ class SearchControllerForPlayerCest extends BaseAcceptance
      * @test
      * @group SearchController
      * @group SearchControllerForPlayer
+     * @param AcceptanceTester $I
      */
     public function testSearchControllerGetAPlayerByFirstNameSearch(AcceptanceTester $I)
     {
-        $faker = \Faker\Factory::create();
         $I->wantTo('Check that we can get a player from the search bar by first name only');
         $I->amOnPage('/app/manage/player');
-        $firstName = $faker->firstName;
-        $lastName = $faker->lastName;
-        $username = $faker->userName;
-        $email = $faker->email;
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $username = $this->faker->userName;
+        $email = $this->faker->email;
         $I->fillField(['id' => 'name'], $firstName . ' ' . $lastName);
         $I->fillField(['id' => 'username'], $username);
         $I->fillField(['id' => 'email'], $email);
         $I->click(['id' => 'submit']);
 
+
+        $I->waitForElementVisible('a[title="Edit player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+//        $id = $I->waitForJS("return document.querySelector('[title=\"Edit player " . $firstName . " " . $lastName ."\"]').id;)", $this::TEXT_WAIT_TIMEOUT);
         /** Do search by first name only */
         $I->fillField(['id' => 'searchText'], $firstName);
         $I->click(['id' => 'doSearchBoxSubmit']);
         $I->waitForElementVisible(['id' => 'searchResults'], BaseAcceptance::TEXT_WAIT_TIMEOUT);
-        $I->wait(1);
-        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForElementVisible('a[title="Player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->click('a[title="Player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForText("Update Player:", $this::TEXT_WAIT_TIMEOUT);
+        $I->see("Update Player: “". $firstName . ' ' . $lastName ."”");
 
     }
 
@@ -166,27 +186,32 @@ class SearchControllerForPlayerCest extends BaseAcceptance
      * @test
      * @group SearchController
      * @group SearchControllerForPlayer
+     * @param AcceptanceTester $I
      */
     public function testSearchControllerGetAPlayerByLastNameSearch(AcceptanceTester $I)
     {
-        $faker = \Faker\Factory::create();
         $I->wantTo('Check that we can get a player from the search bar by last name only');
         $I->amOnPage('/app/manage/player');
-        $firstName = $faker->firstName;
-        $lastName = $faker->lastName;
-        $username = $faker->userName;
-        $email = $faker->email;
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $username = $this->faker->userName;
+        $email = $this->faker->email;
         $I->fillField(['id' => 'name'], $firstName . ' ' . $lastName);
         $I->fillField(['id' => 'username'], $username);
         $I->fillField(['id' => 'email'], $email);
         $I->click(['id' => 'submit']);
 
+        $I->waitForElementVisible('a[title="Edit player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+//        $id = $I->waitForJS("return document.querySelector('[title=\"Edit player " . $firstName . " " . $lastName ."\"]').id;)", $this::TEXT_WAIT_TIMEOUT);
         /** Do search by last name only */
         $I->fillField(['id' => 'searchText'], $lastName);
         $I->click(['id' => 'doSearchBoxSubmit']);
         $I->waitForElementVisible(['id' => 'searchResults'], BaseAcceptance::TEXT_WAIT_TIMEOUT);
-        $I->wait(1);
-        $I->see($firstName . ' ' . $lastName, 'a[title="Edit player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForElementVisible('a[title="Player '. $firstName . ' ' . $lastName .'"]', $this::TEXT_WAIT_TIMEOUT);
+        $I->click('a[title="Player '. $firstName . ' ' . $lastName .'"]');
+        $I->waitForText("Update Player:", $this::TEXT_WAIT_TIMEOUT);
+        $I->see("Update Player: “". $firstName . ' ' . $lastName ."”");
 
     }
 
