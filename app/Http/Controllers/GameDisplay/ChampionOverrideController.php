fabefbcff66447c41ba83f8c601c0fd6066a2fef
romanm <romanm@paulbunyan.net>
2017-09-08 16:47:44 -0500

    commented out api request for champion override

--- app/Http/Controllers/GameDisplay/ChampionOverrideController.php
+++ app/Http/Controllers/GameDisplay/ChampionOverrideController.php
@@ -10,6 +10,20 @@ class ChampionOverrideController extends Controller
 {
     public function pageLoad()
     {
+//        $allChampions = [];
+//        $Url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&dataById=false&api_key='.$_ENV['RIOT_API_KEY1'];
+//        if(Cache::has('AllChampions')){
+//            $champions = Cache::get('AllChampions');
+//        }else{
+//            $info = new Api();
+//            if($champions = $info->apiRequest($Url)){
+//                Cache::put('AllChampions',$champions,1440);
+//                $champions = Cache::get('AllChampions');
+//                foreach($champions->data as $champion){
+//                    array_push($allChampions,$champion->key);
+//                }
+//                sort($allChampions);
+//                return view('/LeagueOfLegends/championOverrideLayout')->with('allChampions',$allChampions);
         $allChampions=[];
         if(file_exists('../../tests/_data/Champions.bin')){
             $data = unserialize(file_get_contents('../../tests/_data/Champions.bin'));
