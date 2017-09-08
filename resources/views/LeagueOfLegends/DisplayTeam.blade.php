5dff7d8fec5f75aa0b9fc7491a7b021655a45ffe
romanm <romanm@paulbunyan.net>
2017-09-08 14:09:25 -0500

    fixed gigazone logo on mobile display switched points and LVL in mobile display added a empty div to bottom of page so that gigazone logo doesnt overlap extra stats in mobile display

--- resources/views/LeagueOfLegends/DisplayTeam.blade.php
+++ resources/views/LeagueOfLegends/DisplayTeam.blade.php
@@ -17,21 +17,21 @@
     @stop
     @section('Player' . $i . 'SoloRank')
         @if(strpos($soloRankArray[$i],"BRONZE" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/bronze.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"SILVER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/1_3.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/silver.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"GOLD" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/goldv.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/gold.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"PLATINUM" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/platinumv.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/platinum.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"DIAMOND" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/diamondi.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/diamond.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"MASTER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/master.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/master.png"/><br/>{{$soloRankArray[$i]}}
         @elseif(strpos($soloRankArray[$i],"CHALLENGER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/challenger.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/challenger.png"/><br/>{{$soloRankArray[$i]}}
         @else
-            <img class="rank unranked" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$soloRankArray[$i]}}
+            <img class="rank unranked" src="/LeagueOfLegendsDisplay/Images/SummonerRank/bronze.png"/><br/>{{$soloRankArray[$i]}}
         @endif
     @stop
     @section('Player' . $i . 'SoloWinLoss')
@@ -39,21 +39,21 @@
     @stop
     @section('Player' . $i . 'FlexRank')
         @if(strpos($flexRankArray[$i],"BRONZE" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/bronze.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"SILVER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/1_3.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/silver.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"GOLD" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/goldv.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/gold.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"PLATINUM" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/platinumv.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/platinum.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"DIAMOND" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/diamondi.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/diamond.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"MASTER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/master.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/master.png"/><br/>{{$flexRankArray[$i]}}
         @elseif(strpos($flexRankArray[$i],"CHALLENGER" )!==false)
-            <img class="rank" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/challenger.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank" src="/LeagueOfLegendsDisplay/Images/SummonerRank/challenger.png"/><br/>{{$flexRankArray[$i]}}
         @else
-            <img class="rank unranked" src="https://www.lol-smurfs.com/blog/wp-content/uploads/2017/01/bronzei.png"/><br/>{{$flexRankArray[$i]}}
+            <img class="rank unranked" src="/LeagueOfLegendsDisplay/Images/SummonerRank/bronze.png"/><br/>{{$flexRankArray[$i]}}
         @endif
     @stop
     @section('Player' . $i . 'FlexWinLoss')
@@ -92,13 +92,13 @@
     @stop
     @section('Player'. $i. 'MasterieRankAndPoints')
         @if($top3ChampionIcons[$i] === false)
-            <div id="imageInfo1{{$i}}" >Summoner level too low to have masteries</div>
-            <div id="imageInfo2{{$i}}" class="hidden">Summoner level too low to have masteries</div>
-            <div id="imageInfo3{{$i}}" class="hidden">Summoner level too low to have masteries</div>
+            <div id="imageInfo1{{$i}}" class="playerInfo">Summoner level too low to have masteries</div>
+            <div id="imageInfo2{{$i}}" class="hidden playerInfo">Summoner level too low to have masteries</div>
+            <div id="imageInfo3{{$i}}" class="hidden playerInfo">Summoner level too low to have masteries</div>
         @else
-            <div id="imageInfo1{{$i}}" ><b class="summonerName">{{explode('/',explode('.',$top3ChampionIcons[$i][0])[4])[3]}}</b><br/><img src="/LeagueOfLegendsDisplay/Images/ChampionMastery/ChampMastery_{{ $top3ChampionRanks[$i][0]}}.png"><br/><div class="collapse"><b class="collapse-M-heading">LVL:</b> {{ $top3ChampionRanks[$i][0]}}</div><div class="collapse"><b class="collapse-M-heading">Points:</b> {{ $top3ChampionPoints[$i][0]}}</div></div>
-            <div id="imageInfo2{{$i}}" class="hidden"><b class="summonerName">{{explode('/',explode('.',$top3ChampionIcons[$i][1])[4])[3]}}</b><br/><img src="/LeagueOfLegendsDisplay/Images/ChampionMastery/ChampMastery_{{ $top3ChampionRanks[$i][1]}}.png"><br/><div class="collapse"><b class="collapse-M-heading">LVL:</b> {{ $top3ChampionRanks[$i][1]}}</div><div class="collapse"><b class="collapse-M-heading">Points:</b> {{ $top3ChampionPoints[$i][1]}}</div></div>
-            <div id="imageInfo3{{$i}}" class="hidden"><b class="summonerName">{{explode('/',explode('.',$top3ChampionIcons[$i][2])[4])[3]}}</b><br/><img src="/LeagueOfLegendsDisplay/Images/ChampionMastery/ChampMastery_{{ $top3ChampionRanks[$i][2]}}.png"><br/><div class="collapse"><b class="collapse-M-heading">LVL:</b> {{ $top3ChampionRanks[$i][2]}}</div><div class="collapse"><b class="collapse-M-heading">Points:</b> {{ $top3ChampionPoints[$i][2]}}</div></div>
+            <div id="imageInfo1{{$i}}" ><b class="summonerName">{{explode('/',explode('.',$top3ChampionIcons[$i][0])[4])[3]}}</b><br/><img src="/LeagueOfLegendsDisplay/Images/ChampionMastery/ChampMastery_{{ $top3ChampionRanks[$i][0]}}.png"><br/><div class="collapse"><b class="collapse-M-heading">Points:</b> {{ $top3ChampionPoints[$i][0]}}</div><div class="collapse"><b class="collapse-M-heading">LVL:</b> {{ $top3ChampionRanks[$i][0]}}</div></div>
+            <div id="imageInfo2{{$i}}" class="hidden"><b class="summonerName">{{explode('/',explode('.',$top3ChampionIcons[$i][1])[4])[3]}}</b><br/><img src="/LeagueOfLegendsDisplay/Images/ChampionMastery/ChampMastery_{{ $top3ChampionRanks[$i][1]}}.png"><br/><div class="collapse"><b class="collapse-M-heading">Points:</b> {{ $top3ChampionPoints[$i][1]}}</div><div class="collapse"><b class="collapse-M-heading">LVL:</b> {{ $top3ChampionRanks[$i][1]}}</div></div>
+            <div id="imageInfo3{{$i}}" class="hidden"><b class="summonerName">{{explode('/',explode('.',$top3ChampionIcons[$i][2])[4])[3]}}</b><br/><img src="/LeagueOfLegendsDisplay/Images/ChampionMastery/ChampMastery_{{ $top3ChampionRanks[$i][2]}}.png"><br/><div class="collapse"><b class="collapse-M-heading">Points:</b> {{ $top3ChampionPoints[$i][2]}}</div><div class="collapse"><b class="collapse-M-heading">LVL:</b> {{ $top3ChampionRanks[$i][2]}}</div></div>
         @endif
     @stop
     @section('Player'.$i.'MasteriesSplashArt')
