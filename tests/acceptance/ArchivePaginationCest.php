a59084180c6f5d2dd8624abe8322f107a204522a
Nate Nolting <me@natenolting.com>
2017-09-14 13:32:48 -0500

    Fixed tests for pagination so they now pass.

--- tests/acceptance/ArchivePaginationCest.php
+++ tests/acceptance/ArchivePaginationCest.php
@@ -12,7 +12,12 @@ class ArchivePaginationCest extends BaseAcceptance
     public function _before(AcceptanceTester $I)
     {
         parent::_before($I);
-        exec('wp post generate --count=20 --path=public_html/wp >> /dev/null 2>&1');
+        $I->loginToWordpress($I, 'admin', 'password', 1);
+        $postsPerPage = $I->grabFromDatabase('wp_options', 'option_value', ['option_name' => 'posts_per_page']);
+        /** Make a bunch of posts so that the pagination will show up */
+        for($i=0; $i < floor(intval($postsPerPage) * 2.5); $i++) {
+            $I->createAPost($I, $this->faker->sentence(), $this->faker->paragraph());
+        }
 
     }
 
@@ -30,31 +35,30 @@ class ArchivePaginationCest extends BaseAcceptance
      * Check that pagination shows on archive pages
      *
      * @param AcceptanceTester $I
+     * @group pagination
+     * @test
      */
-    public function pagination_displays_on_archive(AcceptanceTester $I, $scenario)
+    public function checkThatPaginationShowsOnArchivePages(AcceptanceTester $I, $scenario)
     {
-        $I->wantTo('see that the pagination block shows on a archive list page');
         $I->amOnPage('/'.date('Y').'/'.date('m'));
         $I->seeElement(['class' => 'pagination']);
     }
 
     /**
      * Check that clicking on pagination sends to the right page
-     *
-     * @skip
      * @param AcceptanceTester $I
+     * @group pagination
+     * @test
      */
-    public function pagination_send_to_archive_page_on_click(AcceptanceTester $I, $scenario)
+    public function checkThatClickingOnPaginationSendsToTheRightPage(AcceptanceTester $I, $scenario)
     {
-        $scenario->incomplete("this test was skipt due to errors when going into jenkins");
-        $I->wantTo('see that the pagination block shows on a archive list page');
         $I->amOnPage('/'.date('Y').'/'.date('m'));
         $I->click(['id' => 'pagination-2']);
+        $I->wait(floor(BaseAcceptance::TEXT_WAIT_TIMEOUT / 2));
+        $I->see('(Page 2)');
         $I->dontSee('It looks like nothing was found at this location.');
         $I->see('Archive');
         $I->see(date('F, Y'));
-        $I->see('(Page 2)');
         $I->seeElement('span#pagination-2');
-
     }
 }
