78ca15039debdb7d91bcdce1d2446cfc068203bd
Nelson <castillo@paulbunyan.net>
2017-10-02 10:56:11 -0500

    I am going to start changing all migrations to verify if tables or rows are there(depending of each case) so there is no issue migrating anymore (even if we have the tables and we truncate the migrations table)

--- database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
+++ database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
@@ -30,78 +30,87 @@ class MigrationCartalystSentinel extends Migration
      */
     public function up()
     {
-        Schema::connection('mysql_sentinel')->create('activations', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->string('code');
-            $table->boolean('completed')->default(0);
-            $table->timestamp('completed_at')->nullable();
-            $table->timestamps();
+        if (!Schema::connection('mysql_sentinel')->hasTable('activations')) {
+            Schema::connection('mysql_sentinel')->create('activations', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->unsigned();
+                $table->string('code');
+                $table->boolean('completed')->default(0);
+                $table->timestamp('completed_at')->nullable();
+                $table->timestamps();
 
-            $table->engine = 'InnoDB';
-        });
+                $table->engine = 'InnoDB';
+            });
+        }
 
-        Schema::connection('mysql_sentinel')->create('persistences', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->string('code');
-            $table->timestamps();
+        if (!Schema::connection('mysql_sentinel')->hasTable('persistences')) {
+            Schema::connection('mysql_sentinel')->create('persistences', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->unsigned();
+                $table->string('code');
+                $table->timestamps();
 
-            $table->engine = 'InnoDB';
-            $table->unique('code');
-        });
+                $table->engine = 'InnoDB';
+                $table->unique('code');
+            });
+        }
+        if (!Schema::connection('mysql_sentinel')->hasTable('reminders')) {
+            Schema::connection('mysql_sentinel')->create('reminders', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->unsigned();
+                $table->string('code');
+                $table->boolean('completed')->default(0);
+                $table->timestamp('completed_at')->nullable();
+                $table->timestamps();
+            });
+        }
+        if (!Schema::connection('mysql_sentinel')->hasTable('roles')) {
+            Schema::connection('mysql_sentinel')->create('roles', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('slug');
+                $table->string('name');
+                $table->text('permissions')->nullable();
+                $table->timestamps();
 
-        Schema::connection('mysql_sentinel')->create('reminders', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->string('code');
-            $table->boolean('completed')->default(0);
-            $table->timestamp('completed_at')->nullable();
-            $table->timestamps();
-        });
+                $table->engine = 'InnoDB';
+                $table->unique('slug');
+            });
+        }
+        if (!Schema::connection('mysql_sentinel')->hasTable('role_users')) {
+            Schema::connection('mysql_sentinel')->create('role_users', function (Blueprint $table) {
+                $table->integer('user_id')->unsigned();
+                $table->integer('role_id')->unsigned();
+                $table->nullableTimestamps();
 
-        Schema::connection('mysql_sentinel')->create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('slug');
-            $table->string('name');
-            $table->text('permissions')->nullable();
-            $table->timestamps();
+                $table->engine = 'InnoDB';
+                $table->primary(['user_id', 'role_id']);
+            });
+        }
+        if (!Schema::connection('mysql_sentinel')->hasTable('throttle')) {
+            Schema::connection('mysql_sentinel')->create('throttle', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->unsigned()->nullable();
+                $table->string('type');
+                $table->string('ip')->nullable();
+                $table->timestamps();
 
-            $table->engine = 'InnoDB';
-            $table->unique('slug');
-        });
+                $table->engine = 'InnoDB';
+                $table->index('user_id');
+            });
+        }
+        if (!Schema::connection('mysql_sentinel')->hasTable('users')) {
+            Schema::connection('mysql_sentinel')->create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('email');
+                $table->string('password');
+                $table->text('permissions')->nullable();
+                $table->timestamp('last_login')->nullable();
+                $table->timestamps();
 
-        Schema::connection('mysql_sentinel')->create('role_users', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
-            $table->integer('role_id')->unsigned();
-            $table->nullableTimestamps();
-
-            $table->engine = 'InnoDB';
-            $table->primary(['user_id', 'role_id']);
-        });
-
-        Schema::connection('mysql_sentinel')->create('throttle', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned()->nullable();
-            $table->string('type');
-            $table->string('ip')->nullable();
-            $table->timestamps();
-
-            $table->engine = 'InnoDB';
-            $table->index('user_id');
-        });
-
-        Schema::connection('mysql_sentinel')->create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('email');
-            $table->string('password');
-            $table->text('permissions')->nullable();
-            $table->timestamp('last_login')->nullable();
-            $table->timestamps();
-
-            $table->engine = 'InnoDB';
-            $table->unique('email');
-        });
+                $table->engine = 'InnoDB';
+                $table->unique('email');
+            });
+        }
     }
 
     /**
@@ -111,12 +120,26 @@ class MigrationCartalystSentinel extends Migration
      */
     public function down()
     {
-        Schema::connection('mysql_sentinel')->drop('activations');
-        Schema::connection('mysql_sentinel')->drop('persistences');
-        Schema::connection('mysql_sentinel')->drop('reminders');
-        Schema::connection('mysql_sentinel')->drop('roles');
-        Schema::connection('mysql_sentinel')->drop('role_users');
-        Schema::connection('mysql_sentinel')->drop('throttle');
-        Schema::connection('mysql_sentinel')->drop('users');
+        if (Schema::connection('mysql_sentinel')->hasTable('activations')) {
+            Schema::connection('mysql_sentinel')->drop('activations');
+        }
+        if (Schema::connection('mysql_sentinel')->hasTable('persistences')) {
+            Schema::connection('mysql_sentinel')->drop('persistences');
+        }
+        if (Schema::connection('mysql_sentinel')->hasTable('reminders')) {
+            Schema::connection('mysql_sentinel')->drop('reminders');
+        }
+        if (Schema::connection('mysql_sentinel')->hasTable('roles')) {
+            Schema::connection('mysql_sentinel')->drop('roles');
+        }
+        if (Schema::connection('mysql_sentinel')->hasTable('role_users')) {
+            Schema::connection('mysql_sentinel')->drop('role_users');
+        }
+        if (Schema::connection('mysql_sentinel')->hasTable('throttle')) {
+            Schema::connection('mysql_sentinel')->drop('throttle');
+        }
+        if (Schema::connection('mysql_sentinel')->hasTable('users')) {
+            Schema::connection('mysql_sentinel')->drop('users');
+        }
     }
 }
