cdea970a8541c0eb5ee274a5a56d5d821766d092
Nelson <castillo@paulbunyan.net>
2017-10-02 15:55:09 -0500

    database migration with checks

--- database/migrations/2016_05_12_202509_create_players_table.php
+++ database/migrations/2016_05_12_202509_create_players_table.php
@@ -12,15 +12,17 @@ class CreatePlayersTable extends Migration
      */
     public function up()
     {
-        Schema::connection('mysql_champ')->create('players', function (Blueprint $table) {
-            $table->engine = "InnoDB";
-            $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('name');
-            $table->string('phone');
-            $table->timestamps();
-        });
+        if (!Schema::connection('mysql_champ')->hasTable('players')) {
+            Schema::connection('mysql_champ')->create('players', function (Blueprint $table) {
+                $table->engine = "InnoDB";
+                $table->increments('id');
+                $table->string('username')->unique();
+                $table->string('email')->unique();
+                $table->string('name');
+                $table->string('phone');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
@@ -30,6 +32,8 @@ class CreatePlayersTable extends Migration
      */
     public function down()
     {
-        Schema::connection('mysql_champ')->drop('players');
+        if (Schema::connection('mysql_champ')->hasTable('players')) {
+            Schema::connection('mysql_champ')->drop('players');
+        }
     }
 }
