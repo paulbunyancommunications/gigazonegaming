552c89708e19b45fb29e17fb35b6d145ec783805
Nelson <castillo@paulbunyan.net>
2017-10-02 15:56:07 -0500

    database migration with checks

--- database/migrations/2016_08_25_131036_turn_on_rest_api_plugins.php
+++ database/migrations/2016_08_25_131036_turn_on_rest_api_plugins.php
@@ -14,19 +14,21 @@ class TurnOnRestApiPlugins extends Migration
      */
     public function up()
     {
-        $getOption = DB::table('wp_options')
-            ->where('option_name', 'active_plugins')
-            ->first();
-        if ($getOption) {
-            $option = \App\Models\WpOption::find($getOption->option_id);
-            $list = unserialize($option->option_value);
-            for ($i = 0; $i < count($this->plugins); $i++) {
-                if (!in_array($this->plugins[$i], $list)) {
-                    array_push($list, $this->plugins[$i]);
+        if (Schema::hasTable('wp_options') and Schema::hasColumn('wp_options', 'option_name')) {
+            $getOption = DB::table('wp_options')
+                ->where('option_name', 'active_plugins')
+                ->first();
+            if ($getOption) {
+                $option = \App\Models\WpOption::find($getOption->option_id);
+                $list = unserialize($option->option_value);
+                for ($i = 0; $i < count($this->plugins); $i++) {
+                    if (!in_array($this->plugins[$i], $list)) {
+                        array_push($list, $this->plugins[$i]);
+                    }
                 }
+                $option->option_value = serialize(array_values($list));
+                $option->save();
             }
-            $option->option_value = serialize(array_values($list));
-            $option->save();
         }
     }
 
@@ -37,22 +39,24 @@ class TurnOnRestApiPlugins extends Migration
      */
     public function down()
     {
-        $getOption = DB::table('wp_options')
-            ->where('option_name', 'active_plugins')
-            ->first();
-        if ($getOption) {
-            $option = \App\Models\WpOption::find($getOption->option_id);
-            $list = unserialize($option->option_value);
-            for ($i = 0; $i < count($this->plugins); $i++) {
-                if (in_array($this->plugins[$i], $list)) {
-                    $key = array_search($this->plugins[$i], $list);
-                    if ($key !== false) {
-                        unset($list[$key]);
+        if (Schema::hasTable('wp_options') and Schema::hasColumn('wp_options', 'option_name')) {
+            $getOption = DB::table('wp_options')
+                ->where('option_name', 'active_plugins')
+                ->first();
+            if ($getOption) {
+                $option = \App\Models\WpOption::find($getOption->option_id);
+                $list = unserialize($option->option_value);
+                for ($i = 0; $i < count($this->plugins); $i++) {
+                    if (in_array($this->plugins[$i], $list)) {
+                        $key = array_search($this->plugins[$i], $list);
+                        if ($key !== false) {
+                            unset($list[$key]);
+                        }
                     }
                 }
+                $option->option_value = serialize(array_values($list));
+                $option->save();
             }
-            $option->option_value = serialize(array_values($list));
-            $option->save();
         }
     }
 }
