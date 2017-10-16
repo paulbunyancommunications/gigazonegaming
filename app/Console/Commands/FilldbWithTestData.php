194c1ae4260036f74b1d7af1f1679c8b6f37b67c
Nelson <castillo@paulbunyan.net>
2017-10-16 09:37:57 -0500

    fixed some git conflicts

--- app/Console/Commands/FilldbWithTestData.php
+++ app/Console/Commands/FilldbWithTestData.php
@@ -1,27 +1,29 @@
 <?php
+
 namespace App\Console\Commands;
+
 use App\Models\Championship\Game;
 use App\Models\Championship\Team;
 use App\Models\Championship\Tournament;
 use App\Models\Championship\Player;
 use Illuminate\Console\Command;
+
 class FilldbWithTestData extends Command
 {
-    //todo: fix team assign
-
-
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'test:fillDB';
+
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'fill DB with test summoners';
+
     /**
      * Create a new command instance.
      *
@@ -31,55 +33,63 @@ class FilldbWithTestData extends Command
     {
         parent::__construct();
     }
+
     /**
      * Execute the console command.
      *
-     * @return void
+     * @return mixed
      */
     public function handle()
     {
-
         $this->fillDB();
-        var_dump("This is working");
+        dd("This is working");
     }
+
     public function fillDB(){
-        $Captain = Player::create([
-            'username' => "KingMorpheus2131",
-            'email' => "martushev8@gmail.com",
-            'name' => 'Roman',
-            'phone' => '2182805085']);
-        $CaptainOfTeam = Player::where('username','KingMorpheus2131')->first();
+
+        #Captian
+        $Captian = new Player();
+        $Captian->setAttribute('username', "KingMorpheus2131");
+        $Captian->setAttribute('email', "simoncecer7@gmail.com");
+        $Captian->setAttribute('name', "Simon");
+        $Captian->setAttribute('phone', "6125596450");
+        $Captian->save();
+
         #Team
-        $team = Team::create([
-            'tournament_id' => 1,
-            'name' => 'That Team',
-            'captain' => $CaptainOfTeam->id,
-        ]);
-        $team = Team::where('name', 'That Team')->first();
+        $team = new Team();
+        $team->tournament_id = 1;
+        $team->name =  "Power Rangers";
+
+        #add captain and save the team
+        $team->captain = $Captian->id;
+        $team->save();
+
+
+
         #relations
-        $Captain::createRelation([
-            'player' => $CaptainOfTeam->id,
-            'Game' => 2,
-            'Tournament' => 1,
-            'team' => $team->id,
+        $Captian::createRelation([
+            'player' => $Captian,
+            'game' => 2,
+            'team' => $team,
         ]);
-        $playerUserNameArray = array('Juanpablomontoya', 'ThatBoy18', 'manklar', 'ReckonStuff');
+
+
+        $playerUserNameArray = array('spartan7warrior', 'gigazoneplayer1', 'gigazoneplayer2', 'gigazoneplayer3');
+
         #creat players for team
-        for($i = 0; $i < count($playerUserNameArray); $i++){
-            $player = Player::create([
-                'username' => $playerUserNameArray[$i],
-                'email' => "ready_player_" . $i . "@gigazonegaming.com",
-                'phone' => "2182605085"
-            ]);
+        foreach ($playerUserNameArray as $Player){
+            $player = new Player();
+            $player->username = $Player;
+            $player->email = "simoncecer7@gmail.com";
+            $player->phone = "6125555555";
+            $player->save();
             // attach player to team/tournament/game
-            $playerOnTeam = Player::where('username',$playerUserNameArray[$i])->first();
             $player::createRelation([
-                'player' => $playerOnTeam->id,
-                'Game' => 2,
-                'Tournament' => 1,
-                'team' => $team->id,
+                'player' => $player,
+                'tournament' => 1,
+                'game' => 2,
+                'team' => $team,
             ]);
         }
-
     }
 }
